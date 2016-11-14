b8f88231a4c36e956117668427c09cbc0fcd3050
Alexander <xander19@btinternet.com>
2016-11-14 16:02:16 +0000

    test

--- semantic/src/connect_db.php
+++ semantic/src/connect_db.php
@@ -1,7 +1,47 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Alexander
- * Date: 11/11/2016
- * Time: 01:26
- */
+<?PHP
+
+// connect to your Azure server and select database (remember you connection details are all on the azure portal
+$db = new mysqli(
+    "us-cdbr-azure-southcentral-f.cloudapp.net",
+    "b5051f6adfc120",
+    "c31439e7",
+    "webappalexander" );
+
+// test our connection
+if ($db->connect_errno) {
+    die ('Connection Failed :'.$db->connect_error );
+}
+
+$dom = new DOMDocument("1.0");
+$node = $dom->createElement("markers");
+$parnode = $dom->appendChild($node);
+
+
+$query = "SELECT * FROM markers WHERE 1";
+$result = $db->query($query);
+if (!$result)   {
+    die('nothing in result: ');
+
+}
+
+header("content-type: text/xml");
+
+
+while ($row = $result->fetch_array()) {
+
+    $node = $dom->createElement("marker");
+    $newnode = $parnode->appendChild($node);
+    $newnode->setAttribute("name", $row['name']);
+    $newnode->setAttribute("address", $row['address']);
+    $newnode->setAttribute("lat", $row['lat']);
+    $newnode->setAttribute("lng", $row['lng']);
+    $newnode->setAttribute("type", $row['type']);
+}
+
+$result->close();
+$db->close();
+
+
+echo $dom->saveXML();
+
+?>
---
+++ semantic/src/inc/includes/MapLanding.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.css">
+    <title>Map Landing</title>
+</head>
+<?php include("includes/header.php"); ?>
+
+<h1>
+    <center>Maps</center>
+</h1>
+<div class="ui horizontal section divider">
+    Walking Routes in Porthlethen
+</div>
+<div class="ui equal width grid">
+    <div class="four wide column"></div>
+    <div class="twelve wide column">
+        <body onload="load()">
+        <div id="map" style="width: 600px; height: 350px"></div>
+        </body>
+
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAAsaPQGyO2SHJumHMC2k8RTYfy3z7OXIk"
+                type="text/JavaScript"></script>
+        <script type="text/JavaScript">
+            function load() {
+                var map = new google.maps.Map(document.getElementById("map"), {
+                    center: new google.maps.LatLng(57.061681, -2.129468),
+                    zoom: 14,
+                    mapTypeId: 'roadmap'
+                });
+                downloadUrl("http://webappalexander.azurewebsites.net/week7/locations.php", function (data) {
+                    var xml = data.responseXML;
+                    var markers = xml.documentElement.getElementsByTagName("marker");
+                    for (var i = 0; i < markers.length; i++) {
+                        var point = new google.maps.LatLng(
+                            parseFloat(markers[i].getAttribute("lat")),
+                            parseFloat(markers[i].getAttribute("lng")));
+                        var marker = new google.maps.Marker({
+                            map: map,
+                            position: point
+                        });
+                    }
+                });
+
+            }
+            ;
+
+
+            function downloadUrl(url, callback) {
+                var request = window.ActiveXObject ?
+                    new ActiveXObject('Microsoft.XMLHTTP') :
+                    new XMLHttpRequest;
+
+                request.onreadystatechange = function () {
+                    if (request.readyState == 4) {
+                        callback(request, request.status);
+                    }
+                };
+
+                request.open('GET', url, true);
+                request.send(null);
+            }
+
+        </script>
+
+    </div>
+    <div class="four wide column"></div>
+</div>
+</div>
+
+<html class="container">
+<div class="ui grid">
+    <div class="four wide column"></div>
+    <div class="eight wide column">
+        <h3> Description</h3>
+        <p>The Thames Path is a long distance walking trail, following England's best known river for 184 miles (294
+            Km) as it meanders from its source in the Cotswolds through several rural counties and on into the heart
+            of London.
+        </p>
+        <p>
+            On its way the Trail passes peaceful water meadows rich in wildlife, historic towns and
+            cities and many lovely villages, finishing at the Thames Barrier in Woolwich just a few miles from the
+            sea.</p>
+    </div>
+    <div class="four wide column">
+    </div>
+    <div class="sixteen wide column"></div>
+    <div class="sixteen wide column"></div>
+    <div class="sixteen wide column"></div>
+</div>
+
+<?php include("includes/footer.php"); ?>
+</html>
