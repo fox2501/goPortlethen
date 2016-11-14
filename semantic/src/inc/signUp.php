027d07bf4ba16c67bb18ff6e20a51c6a3b5cc6c8
ross2106 <ross1994@hotmail.co.uk>
2016-11-14 15:14:30 +0000

    Change to css

--- semantic/src/inc/signUp.php
+++ semantic/src/inc/signUp.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel = "stylesheet" type = "text/css" href = "/semantic/dist/semantic.css">
+    <link rel = "stylesheet" type = "text/css" href = "/goPortlethen/semantic/dist/semantic.css">
     <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src = "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.js"></script>
