1d5d6249ff50aa5d6cd61c8c69f7c11e36275405
Lorna <lornam@me.com>
2016-11-20 15:54:13 +0000

    health page (grid spacings)

--- semantic/src/inc/health.php
+++ semantic/src/inc/health.php
@@ -16,23 +16,22 @@
         Keeping Portlethen Healthy
     </div>
 
-    <!-- Submit content button -->
-    <!-- Takes user to SubmitHealth.php -->
-    <!-- Only visible to admin/contributor -->
-    <!-- When ordinary user on site, hide this button -->
     <div class="ui container">
         <div class="ui grid">
             <div class="row">
                 <div class="two wide column">
+                    <!-- Submit content button -->
+                    <!-- Takes user to SubmitHealth.php -->
+                    <!-- Only visible to admin/contributor -->
+                    <!-- When ordinary user on site, hide this button -->
                     <a href="submithealth.php"><button class="ui primary button">Submit Content</button></a>
                     <br><br>
                 </div>
-                <div class="two wide column"></div>
+                <!-- If there is no content on the page, keep calendar on row 1 (with submit content button) -->
+                <!--<div class="two wide column"></div>
                 <div class="three wide column"></div>
-                    <!-- If there is no content on the page, keep calendar on row 1 (with submit content button) -->
-                    <!-- If there is content on the page, keep calendar on row 2 (with section 1 of info) -->
                     <iframe src="https://calendar.google.com/calendar/embed?src=imdvs1dbg4fm5e9g35o2cj8i2g%40group.calendar.google.com&ctz=America/New_York" style="border: 0" width="400" height="300" frameborder="0" scrolling="no"></iframe>
-                </div>
+                </div>-->
             </div>
 
         </div>
@@ -56,6 +55,11 @@
                     <p style="text-align:justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor ante eu faucibus efficitur. Suspendisse potenti. Fusce lobortis massa arcu, eget vestibulum sem sagittis sed. Aliquam nec quam tristique, pulvinar mauris ac, euismod ipsum. Nullam magna ipsum, auctor eget justo a, interdum condimentum tellus. Mauris auctor, massa id pulvinar laoreet, nibh elit fringilla nulla, sed commodo enim velit in massa. Aenean non volutpat orci. Phasellus nunc velit, semper at efficitur ut, finibus vel tellus. Proin egestas quis neque non elementum. Pellentesque eros nulla, venenatis non rhoncus id, blandit eget elit. Duis ornare cursus pretium.</p>
                     <p style="text-align:justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor ante eu faucibus efficitur. Suspendisse potenti. Fusce lobortis massa arcu, eget vestibulum sem sagittis sed. Aliquam nec quam tristique, pulvinar mauris ac, euismod ipsum. Nullam magna ipsum, auctor eget justo a, interdum condimentum tellus. Mauris auctor, massa id pulvinar laoreet, nibh elit fringilla nulla, sed commodo enim velit in massa. Aenean non volutpat orci. Phasellus nunc velit, semper at efficitur ut, finibus vel tellus. Proin egestas quis neque non elementum. Pellentesque eros nulla, venenatis non rhoncus id, blandit eget elit. Duis ornare cursus pretium.</p>
                     <div class="ui divider"></div>
+                </div>
+                <!-- If there is content on the page, keep calendar on row 2 (with section 1 info) -->
+                <div class="two wide column"></div>
+                <div class="three wide column"></div>
+                    <iframe src="https://calendar.google.com/calendar/embed?src=imdvs1dbg4fm5e9g35o2cj8i2g%40group.calendar.google.com&ctz=America/New_York" style="border: 0" width="400" height="300" frameborder="0" scrolling="no"></iframe>
                 </div>
             </div>
 
