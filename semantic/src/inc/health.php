39ce993ea7de655d9c156e45d2558f6b8cca3c60
Lorna <lornam@me.com>
2016-11-20 16:19:22 +0000

    health page (grid spacings)

--- semantic/src/inc/health.php
+++ semantic/src/inc/health.php
@@ -28,7 +28,7 @@
                     <a href="submithealth.php"><button class="ui primary button">Submit Content</button></a>
                     <br><br>
                 </div>
-                <div class="five wide column">
+                <div class="eight wide column">
                     <iframe src="https://calendar.google.com/calendar/embed?src=imdvs1dbg4fm5e9g35o2cj8i2g%40group.calendar.google.com&ctz=America/New_York" style="border: 0" width="400" height="300" frameborder="0" scrolling="no"></iframe>
                 </div>
                 <div class="five wide column">
