a9a15cd010a3a2ada39c6117d8413324b6cc8d49
Lorna <lornam@me.com>
2016-11-20 11:53:35 +0000

    health page update (new grid)

--- semantic/src/inc/health.php
+++ semantic/src/inc/health.php
@@ -32,7 +32,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div><!--
            Div to hold a section of information
                  Div's appear individually as and when contributors add information
             <div>
@@ -43,12 +43,12 @@
                     Edit
                 </button>
                 <div class="ui segment">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor ante eu faucibus efficitur. Suspendisse potenti. Fusce lobortis massa arcu, eget vestibulum sem sagittis sed. Aliquam nec quam tristique, pulvinar mauris ac, euismod ipsum. Nullam magna ipsum, auctor eget justo a, interdum condimentum tellus. Mauris auctor, massa id pulvinar laoreet, nibh elit fringilla nulla, sed commodo enim velit in massa. Aenean non volutpat orci. Phasellus nunc velit, semper at efficitur ut, finibus vel tellus. Proin egestas quis neque non elementum. Pellentesque eros nulla, venenatis non rhoncus id, blandit eget elit. Duis ornare cursus pretium.</p>-->
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor ante eu faucibus efficitur. Suspendisse potenti. Fusce lobortis massa arcu, eget vestibulum sem sagittis sed. Aliquam nec quam tristique, pulvinar mauris ac, euismod ipsum. Nullam magna ipsum, auctor eget justo a, interdum condimentum tellus. Mauris auctor, massa id pulvinar laoreet, nibh elit fringilla nulla, sed commodo enim velit in massa. Aenean non volutpat orci. Phasellus nunc velit, semper at efficitur ut, finibus vel tellus. Proin egestas quis neque non elementum. Pellentesque eros nulla, venenatis non rhoncus id, blandit eget elit. Duis ornare cursus pretium.</p>
                     <img class="ui medium rounded image" src="/images/wireframe/square-image.png">
                 </div>
                 <div class="ui section divider"></div>
             </div>
-        </div>
+        </div>-->
 
         <div class="four wide column">
             <iframe src="https://calendar.google.com/calendar/embed?src=imdvs1dbg4fm5e9g35o2cj8i2g%40group.calendar.google.com&ctz=America/New_York" style="border: 0" width="400" height="300" frameborder="0" scrolling="no"></iframe>
