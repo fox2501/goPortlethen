ae2f835d3f9b38462a5338fb3b601e35618f8cc5
Lorna <lornam@me.com>
2016-11-20 16:54:05 +0000

    health page (scrollable section)

--- semantic/src/inc/health.php
+++ semantic/src/inc/health.php
@@ -56,11 +56,11 @@
 
                     <div class="ui grid">
                         <div class="row">
-                            <div class="two wide column">
+                            <div class="three wide column">
                                 <h3 class="ui header">Section Two</h3>
                                 <img class="ui medium rounded image" src="/images/wireframe/square-image.png">
                             </div>
-                            <div class="six wide column">
+                            <div class="eight wide column">
                                 <!-- Lets user edit title, information and pictures, or delete the whole thing, of already existing health information -->
                                 <!-- Only visible to admin/contributor -->
                                 <!-- When ordinary user is logged in, disable this button -->
@@ -75,11 +75,11 @@
 
                     <div class="ui grid">
                         <div class="row">
-                            <div class="two wide column">
+                            <div class="three wide column">
                                 <h3 class="ui header">Section Three</h3>
                                 <img class="ui medium rounded image" src="/images/wireframe/square-image.png">
                             </div>
-                            <div class="six wide column">
+                            <div class="eight wide column">
                                 <!-- Lets user edit title, information and pictures, or delete the whole thing, of already existing health information -->
                                 <!-- Only visible to admin/contributor -->
                                 <!-- When ordinary user is logged in, disable this button -->
