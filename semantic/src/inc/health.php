63470c1b145d42c0f54427611d0d15bb85acfd66
Lorna <lornam@me.com>
2016-11-20 16:59:08 +0000

    health page (add calendar to RHS)

--- semantic/src/inc/health.php
+++ semantic/src/inc/health.php
@@ -16,14 +16,14 @@
         <p>Keeping Portlethen Healthy</p>
     </div>
 
+    <!-- Submit content button -->
+    <!-- Takes user to SubmitHealth.php -->
+    <!-- Only visible to admin/contributor -->
+    <!-- When ordinary user on site, disable this button -->
     <div class="ui container">
         <div class="ui grid">
             <div class="row">
                 <div class="three wide column">
-                    <!-- Submit content button -->
-                    <!-- Takes user to SubmitHealth.php -->
-                    <!-- Only visible to admin/contributor -->
-                    <!-- When ordinary user on site, disable this button -->
                     <a href="submithealth.php"><button class="ui primary button">Submit Content</button></a>
                     <br><br>
                 </div>
@@ -34,7 +34,10 @@
     <div class=" ui container">
         <div class="ui grid">
             <div class="row">
+                <!-- Scrollable info section -->
                 <div style="overflow: scroll; height:500px; width: 800px">
+
+                    <!-- Section 1 -->
                     <div class="ui grid">
                         <div class="row">
                             <div class="three wide column">
@@ -42,10 +45,6 @@
                                 <img class="ui medium rounded image" src="/images/wireframe/square-image.png">
                             </div>
                             <div class="ten wide column">
-                                <!-- Lets user edit title, information and pictures, or delete the whole thing, of already existing health information -->
-                                <!-- Only visible to admin/contributor -->
-                                <!-- When ordinary user is logged in, disable this button -->
-                                <!-- Links to form page that was originally used, but original details are there to be changed -->
                                 <button class="mini ui button" style="float:right; margin-left:20px">Edit</button>
                                 <p style="text-align:justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor ante eu faucibus efficitur. Suspendisse potenti. Fusce lobortis massa arcu, eget vestibulum sem sagittis sed. Aliquam nec quam tristique, pulvinar mauris ac, euismod ipsum. Nullam magna ipsum, auctor eget justo a, interdum condimentum tellus. Mauris auctor, massa id pulvinar laoreet, nibh elit fringilla nulla, sed commodo enim velit in massa. Aenean non volutpat orci. Phasellus nunc velit, semper at efficitur ut, finibus vel tellus. Proin egestas quis neque non elementum. Pellentesque eros nulla, venenatis non rhoncus id, blandit eget elit. Duis ornare cursus pretium.</p>
                                 <p style="text-align:justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor ante eu faucibus efficitur. Suspendisse potenti. Fusce lobortis massa arcu, eget vestibulum sem sagittis sed. Aliquam nec quam tristique, pulvinar mauris ac, euismod ipsum. Nullam magna ipsum, auctor eget justo a, interdum condimentum tellus. Mauris auctor, massa id pulvinar laoreet, nibh elit fringilla nulla, sed commodo enim velit in massa. Aenean non volutpat orci. Phasellus nunc velit, semper at efficitur ut, finibus vel tellus. Proin egestas quis neque non elementum. Pellentesque eros nulla, venenatis non rhoncus id, blandit eget elit. Duis ornare cursus pretium.</p>
@@ -54,6 +53,7 @@
                         </div>
                     </div>
 
+                    <!-- Section 2 -->
                     <div class="ui grid">
                         <div class="row">
                             <div class="three wide column">
@@ -61,10 +61,6 @@
                                 <img class="ui medium rounded image" src="/images/wireframe/square-image.png">
                             </div>
                             <div class="ten wide column">
-                                <!-- Lets user edit title, information and pictures, or delete the whole thing, of already existing health information -->
-                                <!-- Only visible to admin/contributor -->
-                                <!-- When ordinary user is logged in, disable this button -->
-                                <!-- Links to form page that was originally used, but original details are there to be changed -->
                                 <button class="mini ui button" style="float:right; margin-left:20px">Edit</button>
                                 <p style="text-align:justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor ante eu faucibus efficitur. Suspendisse potenti. Fusce lobortis massa arcu, eget vestibulum sem sagittis sed. Aliquam nec quam tristique, pulvinar mauris ac, euismod ipsum. Nullam magna ipsum, auctor eget justo a, interdum condimentum tellus. Mauris auctor, massa id pulvinar laoreet, nibh elit fringilla nulla, sed commodo enim velit in massa. Aenean non volutpat orci. Phasellus nunc velit, semper at efficitur ut, finibus vel tellus. Proin egestas quis neque non elementum. Pellentesque eros nulla, venenatis non rhoncus id, blandit eget elit. Duis ornare cursus pretium.</p>
                                 <p style="text-align:justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor ante eu faucibus efficitur. Suspendisse potenti. Fusce lobortis massa arcu, eget vestibulum sem sagittis sed. Aliquam nec quam tristique, pulvinar mauris ac, euismod ipsum. Nullam magna ipsum, auctor eget justo a, interdum condimentum tellus. Mauris auctor, massa id pulvinar laoreet, nibh elit fringilla nulla, sed commodo enim velit in massa. Aenean non volutpat orci. Phasellus nunc velit, semper at efficitur ut, finibus vel tellus. Proin egestas quis neque non elementum. Pellentesque eros nulla, venenatis non rhoncus id, blandit eget elit. Duis ornare cursus pretium.</p>
@@ -73,6 +69,7 @@
                         </div>
                     </div>
 
+                    <!-- Section 3 -->
                     <div class="ui grid">
                         <div class="row">
                             <div class="three wide column">
@@ -80,10 +77,6 @@
                                 <img class="ui medium rounded image" src="/images/wireframe/square-image.png">
                             </div>
                             <div class="ten wide column">
-                                <!-- Lets user edit title, information and pictures, or delete the whole thing, of already existing health information -->
-                                <!-- Only visible to admin/contributor -->
-                                <!-- When ordinary user is logged in, disable this button -->
-                                <!-- Links to form page that was originally used, but original details are there to be changed -->
                                 <button class="mini ui button" style="float:right; margin-left:20px">Edit</button>
                                 <p style="text-align:justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor ante eu faucibus efficitur. Suspendisse potenti. Fusce lobortis massa arcu, eget vestibulum sem sagittis sed. Aliquam nec quam tristique, pulvinar mauris ac, euismod ipsum. Nullam magna ipsum, auctor eget justo a, interdum condimentum tellus. Mauris auctor, massa id pulvinar laoreet, nibh elit fringilla nulla, sed commodo enim velit in massa. Aenean non volutpat orci. Phasellus nunc velit, semper at efficitur ut, finibus vel tellus. Proin egestas quis neque non elementum. Pellentesque eros nulla, venenatis non rhoncus id, blandit eget elit. Duis ornare cursus pretium.</p>
                                 <p style="text-align:justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor ante eu faucibus efficitur. Suspendisse potenti. Fusce lobortis massa arcu, eget vestibulum sem sagittis sed. Aliquam nec quam tristique, pulvinar mauris ac, euismod ipsum. Nullam magna ipsum, auctor eget justo a, interdum condimentum tellus. Mauris auctor, massa id pulvinar laoreet, nibh elit fringilla nulla, sed commodo enim velit in massa. Aenean non volutpat orci. Phasellus nunc velit, semper at efficitur ut, finibus vel tellus. Proin egestas quis neque non elementum. Pellentesque eros nulla, venenatis non rhoncus id, blandit eget elit. Duis ornare cursus pretium.</p>
@@ -91,6 +84,10 @@
                             </div>
                         </div>
                     </div>
+                </div>
+                <!-- Calendar section -->
+                <div>
+                    <iframe src="https://calendar.google.com/calendar/embed?src=imdvs1dbg4fm5e9g35o2cj8i2g%40group.calendar.google.com&ctz=America/New_York" style="border: 0" width="400" height="300" frameborder="0" scrolling="no"></iframe>
                 </div>
             </div>
         </div>
@@ -101,8 +98,3 @@
     <?php include("includes/footer.php"); ?>
 </body>
 </html>
-
-<!--
-<div class="row">
-    <iframe src="https://calendar.google.com/calendar/embed?src=imdvs1dbg4fm5e9g35o2cj8i2g%40group.calendar.google.com&ctz=America/New_York" style="border: 0" width="400" height="300" frameborder="0" scrolling="no"></iframe>
-</div>
