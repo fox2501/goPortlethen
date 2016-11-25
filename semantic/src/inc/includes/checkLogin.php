8b673b95f20396a50adbe23ee87c3d0b0385832a
ross2106 <ross1994@hotmail.co.uk>
2016-11-25 11:00:48 +0000

    Changing login page

---
+++ semantic/src/inc/includes/checkLogin.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ross1
+ * Date: 25/11/2016
+ * Time: 10:59
+ */
--- semantic/src/inc/logIn.php
+++ semantic/src/inc/logIn.php
@@ -18,26 +18,29 @@
                     Log-in to your account
                 </div>
             </h2>
-            <form class="ui large form">
-                <div class="ui stacked segment">
-                    <div class="field">
-                        <div class="ui left icon input">
-                            <i class="user icon"></i>
-                            <input type="text" name="email" placeholder="E-mail address">
+            <form action = "checkLogin.php" method = "post">
+                <form class="ui large form">
+                    <div class="ui stacked segment">
+                        <div class="field">
+                            <div class="ui left icon input">
+                                <i class="user icon"></i>
+                                <input type="text" name="email" placeholder="E-mail address">
+                            </div>
                         </div>
-                    </div>
-                    <div class="field">
-                        <div class="ui left icon input">
-                            <i class="lock icon"></i>
-                            <input type="password" name="password" placeholder="Password">
+                        <div class="field">
+                            <div class="ui left icon input">
+                                <i class="lock icon"></i>
+                                <input type="password" name="password" placeholder="Password">
+                            </div>
                         </div>
+                        <div class="ui fluid large blue submit button">Login</div>
                     </div>
-                    <div class="ui fluid large blue submit button">Login</div>
-                </div>
 
-                <div class="ui error message"></div>
+                    <div class="ui error message"></div>
 
+                </form>
             </form>
+
 
             <div class="ui message">
                 New to us? <a href="signUp.php">Sign Up</a>
