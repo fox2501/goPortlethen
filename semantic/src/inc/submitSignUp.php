bfc42b54f7e85b1a45c37e8ede7845dd1cc8182f
ross2106 <ross1994@hotmail.co.uk>
2016-11-26 16:13:41 +0000

    Updating signUpForm.php

--- semantic/src/inc/submitSignUp.php
+++ semantic/src/inc/submitSignUp.php
@@ -8,7 +8,7 @@ $username = $_POST["username"];
 $password = $_POST["password"];
 $age = $_POST["age"];
 
-$sql = "INSERT INTO users (userName, password, emailAddress, displayName, age) values('$username', '$password', '$email', '$displayName', '$age')";
+$sql = "INSERT INTO users (userName, password, emailAddress, displayName, approvalStatus, age) values('$username', '$password', '$email', '$displayName', '0', '$age')";
 
 header("location: /semantic/");
 
