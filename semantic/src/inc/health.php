709862de21e23ec3e92ce5882e50c23079ebb922
Lorna <lornam@me.com>
2016-11-28 17:02:31 +0000

    health (conecting to db)

--- semantic/src/inc/health.php
+++ semantic/src/inc/health.php
@@ -43,8 +43,6 @@
                     <div class="infoInput">
                         <div class="ui grid">
                             <div class="row">
-
-
                                 <ul>
                                     <?php
                                     $sql_query = "SELECT * FROM healthContent";
@@ -64,7 +62,7 @@
                                         echo "<div>
                                             <div class=\"ten wide column\">
                                                 <button class=\"mini ui button\" style=\"float:right; margin-left:20px\">Edit</button>
-                                                <p id=\"mainText\">
+                                                <p id=\"mainText\" style=\"text-align:justify\">
                                                     $mainText
                                                 </p>
                                                 <p id=\"author\"><!-- Automatically add username of author --></p>
@@ -75,92 +73,11 @@
                                     }
                                     ?>
                                 </ul>
-
-
-
-                                
-                                
-
-
-                                
-
-                                
-                                
-                                
-                                
-                                
                             </div>
                         </div>
                     </div>
 
-                    <!-- These are placeholders -->
 
-                    <!-- Section 1 -->
-                    <div class="ui grid">
-                        <div class="row">
-                            <div class="four wide column">
-                                <h3 class="ui header">Section One</h3>
-                                <img class="ui medium rounded image" src="/images/wireframe/square-image.png">
-                            </div>
-                            <div class="ten wide column">
-                                <!-- Edit button -->
-                                <!-- Takes user to SubmitHealth.php -->
-                                <!-- Only visible to admin/contributor -->
-                                <!-- When ordinary user on site, block this div -->
-                                <button class="mini ui button" style="float:right; margin-left:20px">Edit</button>
-                                <p style="text-align:justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor ante eu faucibus efficitur. Suspendisse potenti. Fusce lobortis massa arcu, eget vestibulum sem sagittis sed. Aliquam nec quam tristique, pulvinar mauris ac, euismod ipsum. Nullam magna ipsum, auctor eget justo a, interdum condimentum tellus. Mauris auctor, massa id pulvinar laoreet, nibh elit fringilla nulla, sed commodo enim velit in massa. Aenean non volutpat orci. Phasellus nunc velit, semper at efficitur ut, finibus vel tellus. Proin egestas quis neque non elementum. Pellentesque eros nulla, venenatis non rhoncus id, blandit eget elit. Duis ornare cursus pretium.</p>
-                                <p style="text-align:justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor ante eu faucibus efficitur. Suspendisse potenti. Fusce lobortis massa arcu, eget vestibulum sem sagittis sed. Aliquam nec quam tristique, pulvinar mauris ac, euismod ipsum. Nullam magna ipsum, auctor eget justo a, interdum condimentum tellus. Mauris auctor, massa id pulvinar laoreet, nibh elit fringilla nulla, sed commodo enim velit in massa. Aenean non volutpat orci. Phasellus nunc velit, semper at efficitur ut, finibus vel tellus. Proin egestas quis neque non elementum. Pellentesque eros nulla, venenatis non rhoncus id, blandit eget elit. Duis ornare cursus pretium.</p>
-                                <p>Author goes here</p>
-                                <p>Date goes here</p>
-                                <div class="ui divider"></div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Section 2 -->
-                    <div class="ui grid">
-                        <div class="row">
-                            <div class="four wide column">
-                                <h3 class="ui header">Section Two</h3>
-                                <img class="ui medium rounded image" src="/images/wireframe/square-image.png">
-                            </div>
-                            <div class="ten wide column">
-                                <!-- Edit button -->
-                                <!-- Takes user to SubmitHealth.php -->
-                                <!-- Only visible to admin/contributor -->
-                                <!-- When ordinary user on site, block this div -->
-                                <button class="mini ui button" style="float:right; margin-left:20px">Edit</button>
-                                <p style="text-align:justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor ante eu faucibus efficitur. Suspendisse potenti. Fusce lobortis massa arcu, eget vestibulum sem sagittis sed. Aliquam nec quam tristique, pulvinar mauris ac, euismod ipsum. Nullam magna ipsum, auctor eget justo a, interdum condimentum tellus. Mauris auctor, massa id pulvinar laoreet, nibh elit fringilla nulla, sed commodo enim velit in massa. Aenean non volutpat orci. Phasellus nunc velit, semper at efficitur ut, finibus vel tellus. Proin egestas quis neque non elementum. Pellentesque eros nulla, venenatis non rhoncus id, blandit eget elit. Duis ornare cursus pretium.</p>
-                                <p style="text-align:justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor ante eu faucibus efficitur. Suspendisse potenti. Fusce lobortis massa arcu, eget vestibulum sem sagittis sed. Aliquam nec quam tristique, pulvinar mauris ac, euismod ipsum. Nullam magna ipsum, auctor eget justo a, interdum condimentum tellus. Mauris auctor, massa id pulvinar laoreet, nibh elit fringilla nulla, sed commodo enim velit in massa. Aenean non volutpat orci. Phasellus nunc velit, semper at efficitur ut, finibus vel tellus. Proin egestas quis neque non elementum. Pellentesque eros nulla, venenatis non rhoncus id, blandit eget elit. Duis ornare cursus pretium.</p>
-                                <p>Author goes here</p>
-                                <p>Date goes here</p>
-                                <div class="ui divider"></div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Section 3 -->
-                    <div class="ui grid">
-                        <div class="row">
-                            <div class="four wide column">
-                                <h3 class="ui header">Section Three</h3>
-                                <img class="ui medium rounded image" src="/images/wireframe/square-image.png">
-                            </div>
-                            <div class="ten wide column">
-                                <!-- Edit button -->
-                                <!-- Takes user to SubmitHealth.php -->
-                                <!-- Only visible to admin/contributor -->
-                                <!-- When ordinary user on site, block this div -->
-                                <button class="mini ui button" style="float:right; margin-left:20px">Edit</button>
-                                <p style="text-align:justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor ante eu faucibus efficitur. Suspendisse potenti. Fusce lobortis massa arcu, eget vestibulum sem sagittis sed. Aliquam nec quam tristique, pulvinar mauris ac, euismod ipsum. Nullam magna ipsum, auctor eget justo a, interdum condimentum tellus. Mauris auctor, massa id pulvinar laoreet, nibh elit fringilla nulla, sed commodo enim velit in massa. Aenean non volutpat orci. Phasellus nunc velit, semper at efficitur ut, finibus vel tellus. Proin egestas quis neque non elementum. Pellentesque eros nulla, venenatis non rhoncus id, blandit eget elit. Duis ornare cursus pretium.</p>
-                                <p style="text-align:justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor ante eu faucibus efficitur. Suspendisse potenti. Fusce lobortis massa arcu, eget vestibulum sem sagittis sed. Aliquam nec quam tristique, pulvinar mauris ac, euismod ipsum. Nullam magna ipsum, auctor eget justo a, interdum condimentum tellus. Mauris auctor, massa id pulvinar laoreet, nibh elit fringilla nulla, sed commodo enim velit in massa. Aenean non volutpat orci. Phasellus nunc velit, semper at efficitur ut, finibus vel tellus. Proin egestas quis neque non elementum. Pellentesque eros nulla, venenatis non rhoncus id, blandit eget elit. Duis ornare cursus pretium.</p>
-                                <p>Author goes here</p>
-                                <p>Date goes here</p>
-                                <div class="ui divider"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
                 <!-- Calendar and Twitter section -->
                 <div class="two wide column">
