8c5a8b1c5d8bb92a33ff2a04406c20708ebc38ca
Lorna <lornam@me.com>
2016-11-28 19:11:18 +0000

    health (picture sizes)

--- semantic/src/inc/health.php
+++ semantic/src/inc/health.php
@@ -55,7 +55,7 @@
                                                 <h3 class=\"ui header\" id=\"title\">
                                                     $title
                                                 </h3>
-                                                <img class=\"ui medium rounded image\" style=\"width:\"300px\" height:\"300px\" \" src=\"https://scontent.flhr4-1.fna.fbcdn.net/v/t1.0-9/13434842_1608517786105160_4523080997776743356_n.jpg?oh=4981b2761c2ef40c4989fc4b74bd440a&oe=58C6B38A\"\" id=\"image\"></img>
+                                                <img class=\"ui medium rounded image\" src=\"https://scontent.flhr4-1.fna.fbcdn.net/v/t1.0-9/13434842_1608517786105160_4523080997776743356_n.jpg?oh=4981b2761c2ef40c4989fc4b74bd440a&oe=58C6B38A\"\" id=\"image\"></img>
                                                 
             
                                             </div>
