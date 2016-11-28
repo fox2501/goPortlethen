2289c805bcbd03a7cd8cdefcff5885540d3290d0
Lorna <lornam@me.com>
2016-11-28 16:47:09 +0000

    health (conecting to db)

--- semantic/src/inc/health.php
+++ semantic/src/inc/health.php
@@ -52,7 +52,7 @@
                                     while($row = $result-> fetch_array()) {
                                         $title = $row['title'];
                                         $mainText = $row['mainText'];
-                                        echo "<li> </li>";
+                                        echo "<li> $title $mainText</li>";
                                     }
                                     ?>
                                 </ul>
