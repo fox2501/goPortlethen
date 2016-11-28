fc9ff6ab480e4a948309843912d54ce879f51908
Lorna <lornam@me.com>
2016-11-28 13:44:52 +0000

    health (conecting to db)

--- semantic/src/inc/submitHealthForm.php
+++ semantic/src/inc/submitHealthForm.php
@@ -4,7 +4,7 @@ include("includes/dbconnect.php");
 
 $title = $_POST["title"];
 
-$sql = "INSERT INTO healthContent (title, mainText, userID, contentType) VALUES ('$title', 'NULL', 'NULL', 'NULL')";
+$sql = "INSERT INTO healthContent (title, mainText, userID, contentType, approvalStatus) VALUES ('$title', 'NULL', 'NULL', 'NULL', 0)";
 
 if (mysqli_query($db, $sql)) {
 } else {
