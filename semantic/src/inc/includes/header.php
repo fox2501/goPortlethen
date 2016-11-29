b9b35621a4cb27f5a990fac1bc9497a036c795ff
ross2106 <ross1994@hotmail.co.uk>
2016-11-29 11:08:52 +0000

    Multiple changes

--- semantic/src/inc/includes/header.php
+++ semantic/src/inc/includes/header.php
@@ -33,25 +33,25 @@ session_start();
         <?php
             if(isset($_SESSION['loggedIn'])){
                 echo "  <div class = 'ui simple dropdown'>
-                            <a class = 'browse item' href = 'semantic/src/inc/profile.php'>
+                            <a class = 'browse item' href = '/semantic/src/inc/profile.php'>
                                 <i class = 'user icon'></i>
                                     Ross
                                 <i class='dropdown icon'></i>
                             </a>
                         <div class = 'ui menu'>
-                            <div class = 'item' href = 'semantic/src/inc/profile.php'>My Profile</div>
-                            <div class = 'item' href = 'semantic/src/inc/profile.php'>Settings</div>
+                            <div class = 'item' href = '/semantic/src/inc/profile.php'>My Profile</div>
+                            <div class = 'item' href = '/semantic/src/inc/profile.php'>Settings</div>
                             <div class = 'item' type = 'submit' action = 'logout.php'>Log Out</button>
                         </div>
                         </div>";
             } else {
-                echo "<a class = 'item' href = 'semantic/src/inc/signUpForm.php'>
+                echo "<a class = 'item' href = '/semantic/src/inc/signUpForm.php'>
                         <div class = 'ui teal button'>
                             <i class='add user icon'></i>
                                 Sign up
                         </div>
                       </a>
-                      <a class = 'item' href = 'semantic/src/inc/logIn.php'>
+                      <a class = 'item' href = '/semantic/src/inc/logIn.php'>
                         <div class = 'ui button'>
                             <i class='sign in icon'></i>
                                 Log In
