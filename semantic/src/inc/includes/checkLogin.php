13636ccb49ce0bf0f46e3ede52fd78528e1345e4
ross2106 <ross1994@hotmail.co.uk>
2016-11-29 10:25:45 +0000

    Multiple changes

--- semantic/src/inc/includes/checkLogin.php
+++ semantic/src/inc/includes/checkLogin.php
@@ -1,11 +1,17 @@
 <?php
 
+includes("dbconnect.php");
+
 $username = $_POST["email"];
 $password = $_POST["password"];
 
-if ($username == "email" && $password == "password") {
-    setcookie('access_level_cookie', 'standarduser');
-}
-header('Location: logginIn.php');
+$sql = "SELECT * FROM user where username = '$username' AND password = '$password'";
+$result = mysqli_query($db, $sql);
 
+if (!$row = mysqli_fetch_assoc($result)){
+    echo "Your username or password is incorrect!";
+} else{
+    echo "You are logged in!";
+}
+//header('Location: logginIn.php');
 ?>
