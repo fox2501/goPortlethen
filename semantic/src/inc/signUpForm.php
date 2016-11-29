278125d04e5fc73bcca1cfcdabbfd6ee3d501934
ross2106 <ross1994@hotmail.co.uk>
2016-11-29 11:39:46 +0000

    Adding error handling

--- semantic/src/inc/signUpForm.php
+++ semantic/src/inc/signUpForm.php
@@ -5,56 +5,61 @@ session_start();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel = "stylesheet" type = "text/css" href = "goPortlethen/semantic/dist/semantic.css">
-    <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src = "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.js"></script>
     <title>goPortlethen</title>
 </head>
-<?php include("includes/header.php");?>
+<?php include("includes/header.php"); ?>
+<?php $url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+echo $url;
+?>
+
 <body>
-<div class = "ui container">
-    <form class = "ui form" action = "includes/submitSignUp.php" method = "POST">
+<div class="ui container">
+    <form class="ui form" action="includes/submitSignUp.php" method="POST">
         <h2 class="ui center aligned blue header">
             <div class="content">
                 Sign up
             </div>
         </h2>
-        <div class = "ui segment">
-        <div class = "field">
-            <label>Display Name</label>
-            <div class = "field">
-                <input type = "text" name = "displayName" placeholder = "Enter your display name. This can just be your first name.">
-            </div>
-        </div>
-        <div class = "field">
-            <label>Email Address</label>
-            <div class = "field">
-                <input type = "text" name = "email" placeholder = "Please enter your email address.">
-            </div>
-        </div>
-        <div class = "field">
-            <label>Username</label>
-            <div class = "field">
-                <input type = "text" name = "username" placeholder = "Please enter a username. You will use this to login.">
-            </div>
-        </div>
-        <div class = "field">
-            <label>Password</label>
-            <div class = "field">
-                <input type = "password" name = "password" placeholder = "Please create a password.">
-            </div>
-        </div>
-            <div class = "field">
-                <label>Age</label>
-                <div class = "field">
-                    <input type = "number" name = "age" placeholder = "Please enter your age."
+        <div class="ui segment">
+            <div class="field">
+                <label>Display Name</label>
+                <div class="field">
+                    <input type="text" name="displayName"
+                           placeholder="Enter your display name. This can just be your first name.">
                 </div>
             </div>
-            <button class="ui fluid large green submit button" type = "submit">Create Account</button>
+            <div class="field">
+                <label>Email Address</label>
+                <div class="field">
+                    <input type="text" name="email" placeholder="Please enter your email address.">
+                </div>
+            </div>
+            <div class="field">
+                <label>Username</label>
+                <div class="field">
+                    <input type="text" name="username"
+                           placeholder="Please enter a username. You will use this to login.">
+                </div>
+            </div>
+            <div class="field">
+                <label>Password</label>
+                <div class="field">
+                    <input type="password" name="password" placeholder="Please create a password.">
+                </div>
+            </div>
+            <div class="field">
+                <label>Age</label>
+                <div class="field">
+                    <input type="number" name="age" placeholder="Please enter your age."
+                </div>
+            </div>
+            <button class="ui fluid large green submit button" type="submit">Create Account</button>
         </div>
     </form>
 </div>
 </body>
-<?php include("includes/footer.php");?>
+<?php include("includes/footer.php"); ?>
 </html>
