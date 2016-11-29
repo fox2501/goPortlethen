82f2f40b17fc22d85f02baab592618c87ee806b0
ross2106 <ross1994@hotmail.co.uk>
2016-11-29 11:35:36 +0000

    Adding error handling

--- semantic/src/inc/includes/submitSignUp.php
+++ semantic/src/inc/includes/submitSignUp.php
@@ -8,6 +8,26 @@ $username = $_POST["username"];
 $password = $_POST["password"];
 $age = $_POST["age"];
 
+if(empty($displayName)){
+    header("Location: ../signUpForm.php?error=empty");
+    exit();
+}
+if(empty($email)){
+    header("Location: ../signUpForm.php?error=empty");
+    exit();
+}
+if(empty($username)){
+    header("Location: ../signUpForm.php?error=empty");
+    exit();
+}
+if(empty($password)){
+    header("Location: ../signUpForm.php?error=empty");
+    exit();
+}
+if(empty($age)){
+    header("Location: ../signUpForm.php?error=empty");
+    exit();
+}
 
 $sql = "INSERT INTO users (userName, password, emailAddress, displayName, approvalStatus, age) 
 values('$username', '$password', '$email', '$displayName', '0', '$age')";
