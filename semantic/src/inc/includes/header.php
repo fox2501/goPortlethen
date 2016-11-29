d73907b26af73850fdf08fb71b028c651c56983e
fox2501 <liamstewart777@hotmail.co.uk>
2016-11-29 22:56:35 +0000

    fixed main page

--- semantic/src/inc/includes/header.php
+++ semantic/src/inc/includes/header.php
@@ -1,12 +1,7 @@
 <?
 session_start();
 ?>
-<?php
-require 'goPortlethen/semantic/cloudinaryAPI/Cloudinary.php';
-require 'goPortlethen/semantic/cloudinaryAPI/Uploader.php';
-require 'goPortlethen/semantic/cloudinaryAPI/Api.php';
 
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -21,6 +16,12 @@ require 'goPortlethen/semantic/cloudinaryAPI/Api.php';
     <script type="text/javascript" src="jquery.iframe-transport.js"></script>
     <script type="text/javascript" src="jquery.fileupload.js"></script>
     <script type="text/javascript" src="jquery.cloudinary.js"></script>
+    <?php
+    require 'goPortlethen/semantic/cloudinaryAPI/Cloudinary.php';
+    require 'goPortlethen/semantic/cloudinaryAPI/Uploader.php';
+    require 'goPortlethen/semantic/cloudinaryAPI/Api.php';
+
+    ?>
 <!--    $.cloudinary.config({ cloud_name: 'goportlethencs8', api_key: '116467215234765'})-->
     <title>goPortlethen</title>
 </head>
