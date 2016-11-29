4b6de294c225010d8ddada44b05d2d9c8635bacd
Liam Stewart <liamstewart777@hotmail.co.uk>
2016-11-29 12:16:35 +0000

    password hashing

--- semantic/src/inc/includes/loginHash.php
+++ semantic/src/inc/includes/loginHash.php
@@ -4,14 +4,13 @@ include includes("/dbconnect.php");
 
 
 
-$salt = random_bytes(16);
+
 $hash = password_hash($userPassword,PASSWORD_DEFAULT);
-$hashkey =  $salt +$hash;
 
 
-if (password_verify($hashkey,$hash,['cost' => 12])){
+if (password_verify($hash,$hash,['cost' => 12])){
     //Login Successful
-    if(password_needs_hash($hashkey,PASSWORD_DEFAULT,['cost' => 12])){
+    if(password_needs_hash($hash,PASSWORD_DEFAULT,['cost' => 12])){
 
     }
 }
