8f141f9e370ff1f78a6c6146ade46fee4380ee74
fox2501 <liamstewart777@hotmail.co.uk>
2016-11-29 16:29:31 +0000

    changed login to display name

--- semantic/src/inc/includes/header.php
+++ semantic/src/inc/includes/header.php
@@ -35,7 +35,7 @@ session_start();
                 echo "  <div class = 'ui simple dropdown'>
                             <a class = 'browse item' href = '/semantic/src/inc/profile.php'>
                                 <i class = 'user icon'></i>
-                                    Ross
+                                " .$_SESSION['name']. "
                                 <i class='dropdown icon'></i>
                             </a>
                         <div class = 'ui menu'>
