fa0940cbeb0e5da2cfddd2ba25a08c8baa1eeead
1301118 <1301118@rgu.ac.uk>
2016-11-30 15:53:56 +0000

    fixing validation

--- semantic/src/inc/createClubPage.php
+++ semantic/src/inc/createClubPage.php
@@ -131,7 +131,7 @@
     </form>
 
     <script type="text/javascript">
-        $(".ui.form")
+        $('.ui.form')
             .form({
                     email: {
                         identifier: 'email',
