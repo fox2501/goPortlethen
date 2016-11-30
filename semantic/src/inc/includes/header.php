098bf724332dc684836a7357d19f30dbc0aa63b4
fox2501 <liamstewart777@hotmail.co.uk>
2016-11-30 12:37:08 +0000

    fix include for cloudinary

--- semantic/src/inc/includes/header.php
+++ semantic/src/inc/includes/header.php
@@ -15,13 +15,14 @@ session_start();
     <script type="text/javascript" src="jquery.ui.widget.js"></script>
     <script type="text/javascript" src="jquery.iframe-transport.js"></script>
     <script type="text/javascript" src="jquery.fileupload.js"></script>
-    <script type="text/javascript" src="jquery.cloudinary.js"></script>
+    <script type="text/javascript" src="jquery.cloudinary.js">$.cloudinary.config({ cloud_name: 'goportlethencs8', api_key: '116467215234765'})</script>
 
     <?php include 'goPortlethen/semantic/cloudinaryAPI/Cloudinary.php';
     include 'goPortlethen/semantic/cloudinaryAPI/Uploader.php';
-    include 'goPortlethen/semantic/cloudinaryAPI/Api.php'; ?>
+    include 'goPortlethen/semantic/cloudinaryAPI/Api.php';
+    ?>
 
-<!--    $.cloudinary.config({ cloud_name: 'goportlethencs8', api_key: '116467215234765'})-->
+
     <title>goPortlethen</title>
 </head>
 <body>
