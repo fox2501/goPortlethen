b833e9deacc919ae7b47a7225fca92c8732e5b12
ross2106 <ross1994@hotmail.co.uk>
2016-12-03 18:55:05 +0000

    Profile page

--- semantic/src/inc/includes/submitEditProfile.php
+++ semantic/src/inc/includes/submitEditProfile.php
@@ -11,13 +11,10 @@ $location = $_POST["location"];
 $aboutUser = $_POST["aboutUser"];
 $userID = $_SESSION['loggedIn'];
 
-//$sql = "INSERT INTO users (userName, password, emailAddress, firstName, surname, approvalStatus, age, dateCreated)
-       //values('$username', '$hashpass', '$email', '$firstName', '$surname', '0', '$age', CURRENT_TIMESTAMP)";
-
 $sql = "UPDATE users SET firstName = $firstName, surname = $surname, age = $age, location = $location, aboutUser = $aboutUser
         WHERE userID = $userID";
 
 $result = mysqli_query($db, $sql);
 
-header("location: ../editProfile.php");
+header("location: ../profile.php");
 ?>
