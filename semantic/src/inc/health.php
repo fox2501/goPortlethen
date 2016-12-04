739aab685e003b8856a02f72080e42effd7d8c5a
Lorna <lornam@me.com>
2016-12-04 18:50:50 +0000

    health form validation

--- semantic/src/inc/health.php
+++ semantic/src/inc/health.php
@@ -90,7 +90,6 @@ session_start();
                                                 $date = date_create();
                                                 echo date_format($date, 'U = Y-m-d H:i:s') . \"\n\";
                                                 ?>
-                                            
                                             </p>
                                             <div class=\"ui divider\"></div>
                                         </div>
