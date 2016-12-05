8485a439a014914c56d0c30c3b223ccf86c4ad99
Lorna <lornam@me.com>
2016-12-05 10:54:55 +0000

    health hide kanye

--- semantic/src/inc/health.php
+++ semantic/src/inc/health.php
@@ -32,10 +32,10 @@ session_start();
             <div class="row">
                 <a href="healthForm.php"><button class="ui primary button" style="margin-right:50px">Submit Content</button></a>
                 <iframe style="margin-right:50px" src="https://calendar.google.com/calendar/embed?src=imdvs1dbg4fm5e9g35o2cj8i2g%40group.calendar.google.com&ctz=America/New_York" style="border: 0" width="400" height="300" frameborder="0" scrolling="no"></iframe>
-                <div style="width:300px">
+                <!--<div style="width:300px">
                     <a class="twitter-timeline" tweetLimit="1" href="https://twitter.com/kanyewest">Tweets by kanyewest</a>
                     <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-                </div>
+                </div>-->
             </div>
         </div>
         <div class="ui divider"></div>
