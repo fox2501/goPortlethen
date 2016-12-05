aff2ea5a4da9df576bc278b32d575de4164d83cb
Liam Stewart <liamstewart777@hotmail.co.uk>
2016-12-05 16:06:15 +0000

    fixclub

--- semantic/src/inc/testPictureUpload.php
+++ semantic/src/inc/testPictureUpload.php
@@ -30,4 +30,10 @@ if(isset($_POST['submit'])){
             echo $pms['data']['error'];
         }
     }
+    $sql1 = "INSERT INTO photos (caption,url,clubID,locationID,healthContentID,routeID) VALUES ('test',$url,NULL,NULL,NULL,NULL)";
+
+    if (mysqli_query($db, $sql1)) {
+    } else {
+        echo "Error: " . $sql1 . "<br>" . mysqli_error($db);
+    }
 }
