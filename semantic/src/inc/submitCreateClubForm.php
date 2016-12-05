c598feb620f81f30e8fc99994b6fe05641da3d27
Alexander <xander19@btinternet.com>
2016-12-05 13:41:39 +0000

    changed redirection of completed submission

--- semantic/src/inc/submitCreateClubForm.php
+++ semantic/src/inc/submitCreateClubForm.php
@@ -1,30 +1,21 @@
 <?php   
 session_start();
-  
-include("includes/dbconnect.php");
+include("..includes/dbconnect.php");
   
 $clubName = $_POST["clubName"];
- 
 $email = $_POST["email"];
- 
 $clubCategory = $_POST["clubCategory"];
- 
 $clubDescription = $_POST["clubDescription"];
- 
 $phoneNumber = $_POST["phoneNumber"];
-
 $contactNumber = $_POST["contactNumber"];
-
 $calendarID = $_POST["calendarID"];
- 
 $feePaid = $_POST["feePaid"];
- 
 $feeCost = $_POST["feeCost"];
  
-$userID = $_SESSION['loggedIn'];
   /*$imageID = new PreloadedFile($_POST['imageID']); if (!$imageID->is_valid()) {    echo "Invalid upload signature"; } else {    $photo->image_identifier = $imageID->identifier(); }*/
   
-$sql = "INSERT INTO club (clubName, clubDescription, email, contactNumber, calendarID, feePaid, feeCost, url, clubCategory) 
+$sql = "
+INSERT INTO club (clubName, clubDescription, email, contactNumber, calendarID, feePaid, feeCost, url, clubCategory) 
     VALUES ('$clubName', '$clubDescription', '$email', '$contactNumber', '$calendarID', '$feePaid', '$feeCost', '', '$clubCategory')";
   
 $result = mysqli_query($db, $sql);
