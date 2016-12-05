4584fb823933b600310d4c44d320ebaecedb24e4
Lorna <lornam@me.com>
2016-12-05 13:12:33 +0000

    health form date added

--- semantic/src/inc/submitHealthForm.php
+++ semantic/src/inc/submitHealthForm.php
@@ -7,12 +7,13 @@ include("includes/dbconnect.php");
 $title = $_POST["title"];
 $mainText = $_POST["mainText"];
 $userID = $_SESSION["loggedIn"];
+$datePosted = $_POST["datePosted"];
 
 if(isset($_SESSION['loggedIn'])) {
     echo "exists";
 }
 
-$sql = "INSERT INTO healthcontent (title, mainText, userID, approvalStatus, datePosted) VALUES ('$title', '$mainText', '$userID', '0', <?php echo date(\"l jS \of F Y h:i:s A\")?>)";
+$sql = "INSERT INTO healthcontent (title, mainText, userID, approvalStatus, datePosted) VALUES ('$title', '$mainText', '$userID', '0', '$datePosted')";
 
 if (mysqli_query($db, $sql)) {
 } else {
