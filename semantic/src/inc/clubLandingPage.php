237b07b52d73e79c45a0c935752885cb3aca82cd
ross2106 <ross1994@hotmail.co.uk>
2016-12-07 11:20:52 +0000

    Club page

--- semantic/src/inc/clubLandingPage.php
+++ semantic/src/inc/clubLandingPage.php
@@ -110,7 +110,7 @@ if(isset($_SESSION['loggedIn'])) {
                    <p> $mainText </p>
             </div>
             <div class='extra' >
-            <form method = 'POST'>
+            <form class = 'ui form' method = 'POST'>
                    <button class='ui right floated button' type = 'submit'>
                       <input type = 'hidden' name = 'viewClub' value = '$clubID'>
                       <a href='/semantic/src/inc/clubPage.php'>For more info click here!</a>
