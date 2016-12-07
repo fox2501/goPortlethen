c70d10bc2edf1ddf4c6d2724e0ab3756410ca96a
ross2106 <ross1994@hotmail.co.uk>
2016-12-07 13:41:58 +0000

    Club page

--- semantic/src/inc/clubPage.php
+++ semantic/src/inc/clubPage.php
@@ -2,10 +2,6 @@
 session_start();
 include("includes/dbconnect.php");
 $clubID = $_POST['viewClub'];
-echo "I am testing to see if this works $clubID";
-if(!(isset($_POST['viewClub']))){
-    echo "no club";
-}
 $sql = "SELECT * FROM club WHERE clubID = '$clubID'";
 $result = $db->query($sql);
 while ($row = $result->fetch_array()) {
@@ -59,24 +55,7 @@ while ($row = $result->fetch_array()) {
                     Club Description:
                 </h5>
                 <div class='ui attached segment'>
-                    <p>Manchester, England. Founded in 1880 as St. Mark's (West
-                        Gorton), they became Ardwick Association Football Club in 1887 and Manchester City in
-                        1894. The club moved
-                        to the City of Manchester Stadium in 2003, having played at Maine Road since 1923.
-
-                        The club's most successful period was in the late 1960s and early 1970s when they won
-                        the League
-                        Championship, FA Cup, League Cup and European Cup Winners' Cup under the management team
-                        of Joe Mercer and
-                        Malcolm Allison. After losing the 1981 FA Cup Final, the club went through a period of
-                        decline, culminating
-                        in relegation to the third tier of English football for the only time in their history
-                        in 1998. Having
-                        regained their Premier League status in the early 2000s, the club was purchased in 2008
-                        by Abu Dhabi United
-                        Group and has become one of the wealthiest in the world. Since 2011 the club have won
-                        six major honours,
-                        including the Premier League in 2012 and 2014.</p>
+                    <p>$clubDesc</p>
                 </div>
                 <h5 class='ui attached header'>
                     Location:
