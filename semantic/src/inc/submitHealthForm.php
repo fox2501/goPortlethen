6a315fd9577fce93268874031bc7d1f7215e2012
ross2106 <ross1994@hotmail.co.uk>
2016-12-07 23:44:19 +0000

    Edit health page

--- semantic/src/inc/submitHealthForm.php
+++ semantic/src/inc/submitHealthForm.php
@@ -8,7 +8,7 @@ $title = $_POST["title"];
 $mainText = $_POST["mainText"];
 $userID = $_SESSION["loggedIn"];
 
-$img = $_FILES['healthPhoto'];
+$img = $_POST['healthPhoto'];
 if(empty($img)){
     echo "<h2>An Image Please.</h2>";
 }else{
