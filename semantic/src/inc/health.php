e1642047785a360ebd67a9e90c702363b08d8ff8
ross2106 <ross1994@hotmail.co.uk>
2016-12-07 13:17:37 +0000

    Club page

--- semantic/src/inc/health.php
+++ semantic/src/inc/health.php
@@ -76,7 +76,13 @@ if(isset($_SESSION['loggedIn'])){
                             echo "
                                 <div class='four wide column'>
                                     <div class = 'ui raised segment'>
-                                        <h3 class='ui header' id='title'>
+                                    <form class = 'ui form' method = 'POST' action = 'editHealthContent.php'>
+                                        <button class = 'ui right floated mini button' onclick = '/semantic/src/inc/editHealthContent.php' type = 'submit'>
+                                            <input type = 'hidden' name = 'editHealth' value = $healthContentID>
+                                            Edit
+                                        </button>
+                                    </form>
+                                    <h3 class='ui header' id='title'>
                                                 $title
                                     </h3>
                                     <p id='datePosted'>$datePosted
@@ -84,13 +90,6 @@ if(isset($_SESSION['loggedIn'])){
                                     </p>
                                     <img class='ui small image' src='https://scontent.flhr4-1.fna.fbcdn.net/v/t1.0-9/13434842_1608517786105160_4523080997776743356_n.jpg?oh=4981b2761c2ef40c4989fc4b74bd440a&oe=58C6B38A' id='image'></img>
                                     <br >
-                                    <form class = 'ui form' method = 'POST' action = 'editHealthContent.php'>
-                                        <button class = 'ui right floated mini button' onclick = '/semantic/src/inc/editHealthContent.php' type = 'submit'>
-                                            <input type = 'hidden' name = 'editHealth' value = $healthContentID>
-                                            Edit
-                                        </button>
-                                    </form>
-
                                     <p id = 'mainText'style = 'text - align:justify' > $mainText
                                         <br >
                                     </p >
