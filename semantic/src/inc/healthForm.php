3626359ae2225674ed3eefa545ed5e22690cf9cb
ross2106 <ross1994@hotmail.co.uk>
2016-12-07 23:43:17 +0000

    Edit health page

--- semantic/src/inc/healthForm.php
+++ semantic/src/inc/healthForm.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include("includes/dbconnect.php");
-if(isset($_SESSION['loggedIn'])){
+if (isset($_SESSION['loggedIn'])) {
     $userID = $_SESSION['loggedIn'];
     $canAccess = '0';
     $sql = "SELECT userName from users WHERE userID = '$userID'";
@@ -13,25 +13,25 @@ if(isset($_SESSION['loggedIn'])){
     $result = mysqli_query($db, $sql);
     $row = mysqli_fetch_assoc($result);
     $accessID = $row["accessID"];
-    if($accessID == '1' || $accessID == '4'){
+    if ($accessID == '1' || $accessID == '4') {
         $canAccess = '1';
-    } else{
+    } else {
         $canAccess = '0';
     }
-?>
+    ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel = "stylesheet" type = "text/css" href = "semantic.css">
-    <title>Semantic UI</title>
-</head>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="semantic.css">
+        <title>Semantic UI</title>
+    </head>
 
-<!-- Nav bar -->
-<?include ("includes/header.php"); ?>
+    <!-- Nav bar -->
+    <? include("includes/header.php"); ?>
 
-<body>
+    <body>
     <!-- Page header -->
     <h1 align="center">Submit Health & Wellbeing Information</h1>
     <div class="ui horizontal section divider">
@@ -40,32 +40,35 @@ if(isset($_SESSION['loggedIn'])){
 
     <!-- Form -->
     <div class="ui container">
-        <form name="healthForm" class="ui form" action="submitHealthForm.php" method="POST" onsubmit="return validateForm()">
+        <form name="healthForm" class="ui form" action="submitHealthForm.php" method="POST"
+              onsubmit="return validateForm()">
 
 
-
-            <label>Title</label>
             <div class="field">
+                <label>Title</label>
                 <input type="text" name="title" placeholder="Enter the title of your post">
             </div>
 
-            <label>Content</label>
+
             <div class="field">
+                <label>Content</label>
                 <textarea rows="8" type="text" name="mainText" placeholder="Enter the content of your post"></textarea>
             </div>
 
-            <label>Main Image</label>
+
             <div class="field">
+                <label>Main Image</label>
                 <div class="ui fluid action input">
                     <input name="healthPhoto" size="35" type="file"/>
                 </div>
             </div>
-            <button class="ui fluid large green submit button" id="createHealthContent" type="submit">Submit Content</button>
+            <button class="ui fluid large green submit button" id="createHealthContent" type="submit">Submit Content
+            </button>
         </form>
     </div>
-</body>
-<!-- Footerccc -->
-<?php include("includes/footer.php"); ?>
+    </body>
+    <!-- Footerccc -->
+    <?php include("includes/footer.php"); ?>
 
-</html>
+    </html>
 <?php }; ?>
