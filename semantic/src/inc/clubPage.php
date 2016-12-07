860a468c20c209679b1105d2b0f8dd7f2b3912ea
ross2106 <ross1994@hotmail.co.uk>
2016-12-07 10:23:58 +0000

    Club page

--- semantic/src/inc/clubPage.php
+++ semantic/src/inc/clubPage.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
+include("includes/dbconnect.php");
+$url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+echo $url;
 ?>
-
-<!-- Database -->
-<? include("includes/dbconnect.php"); ?>
 
 <!DOCTYPE html>
 <html lang="en">
