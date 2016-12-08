6dd2af116b775250eaf096f8bc9a0225cffad3e3
Liam Stewart <liamstewart777@hotmail.co.uk>
2016-12-08 15:50:08 +0000

    edithealthpage

--- semantic/src/inc/submitEditClub.php
+++ semantic/src/inc/submitEditClub.php
@@ -1,6 +1,10 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
-include("includes/dbconnect.php");
+include("includes/PDOConnect.php");
 
 $clubName = $_POST["editClubName"];
 $clubID = $_POST["clubToEdit"];
@@ -12,22 +16,55 @@ $feeCost = $_POST["editCost"];
 
 //FEE REQUIRED DOES NOT WORK
 
-$sql = "
-UPDATE club
-SET clubName = '$clubName',
-clubDescription = '$clubDesc',
-contactNumber = '$contactNum',
-websiteURL = '$websiteURL',
-feePaid = '1',
-feeCost = '$feeCost'
-WHERE clubID = '$clubID';
+$sql = "UPDATE club SET clubName = ?,
+clubDescription =?,
+contactNumber = ?,
+websiteURL = ?,
+feePaid = ?,
+feeCost = ?
+WHERE clubID = ?;
 ";
-$result = mysqli_query($db, $sql);
+$pdo->prepare($sql)->execute($clubName,$clubDesc,$contactNum,$websiteURL,1,$feeCost,$clubID);
 
-if($result){
-    header('Location: /semantic/src/inc/clubLandingPage.php');
-} else{
-    echo "Edit club failed.";
+$img = $_FILES['img'];
+if(empty($img)){
+    echo "<h2>An Image Please.</h2>";
+}else{
+    $filename = $img['tmp_name'];
+    $client_id="54dbcfe7d59ad73";
+    $handle = fopen($filename, "r");
+    $data = fread($handle, filesize($filename));
+    $pvars   = array('image' => base64_encode($data));
+    $timeout = 30;
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_URL, 'https://api.imgur.com/3/image.json');
+    curl_setopt($curl, CURLOPT_TIMEOUT, $timeout);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Client-ID ' . $client_id));
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $pvars);
+    $out = curl_exec($curl);
+    curl_close ($curl);
+    $pms = json_decode($out,true);
+    $url=$pms['data']['link'];
 }
+if(empty($url)){
+
+}
+else{
+    $sql3 = "DELETE FROM photos WHERE clubID =?";
+    $pdo->prepare($sql3)->execute([$clubID]);
+    usleep(10000);
+    $caption = 'test';
+    $sql1 = "INSERT INTO photos(caption,url,clubID,locationID,healthContentID,routeID) VALUES(?,?,?,?,?,?)";
+    $pdo->prepare($sql1)->execute([$caption,$url,$clubID,0,0,0]);
+
+
+}
+
+
+
+header('Location: /semantic/src/inc/clubLandingPage.php');
 
 ?>
