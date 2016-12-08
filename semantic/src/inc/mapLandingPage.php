992048223feaa29f72fc518783f316f5bef6b5c8
ross2106 <ross1994@hotmail.co.uk>
2016-12-08 22:52:12 +0000

    health approvals

--- semantic/src/inc/mapLandingPage.php
+++ semantic/src/inc/mapLandingPage.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 ?>
-<?php include("includes/header.php");?>
+<?php include("includes/header.php"); ?>
 <body>
 <h1 align="center">Map Landing Page</h1>
 <div class="ui horizontal section divider">
@@ -9,22 +9,21 @@ session_start();
 </div>
 <div class='ui container'>
     <div class='ui grid'>
-        <div class='sixteen wide column'>
+        <div class='row'>
             <button class="ui left floating positive fluid button"><a href="">Create Route</a></button>
         </div>
-        <div class='row'>
-            <div class = 'sixteen wide column'
-            <div class='ui raised segment'>
-                <div class="item">
-                    <i class="marker icon"></i>
-                    <div class="content">
-                        <a class="header"></a><a href="mapPage.php">Test Route 1</a>
-                    </div>4.2miles
+        <div class='sixteen wide column'
+        <div class='ui raised segment'>
+            <div class="item">
+                <i class="marker icon"></i>
+                <div class="content">
+                    <a class="header"></a><a href="mapPage.php">Test Route 1</a>
                 </div>
-            </div>
+                4.2miles
             </div>
         </div>
     </div>
 </div>
-<?php include("includes/footer.php");?>
+</div>
+<?php include("includes/footer.php"); ?>
 </body>
