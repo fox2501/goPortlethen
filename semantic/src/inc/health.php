2f46983bbff9dbe1485d6aeffc48add1e5638372
ross2106 <ross1994@hotmail.co.uk>
2016-12-08 00:15:07 +0000

    Edit health page

--- semantic/src/inc/health.php
+++ semantic/src/inc/health.php
@@ -86,38 +86,38 @@ AND A.healthContentID = C.healthContentID";
 <div class='ui raised segment'>
     <div class='ui container'>
         <div class='ui grid'>
-        <div class = 'row'>
             <form class='ui form' method='POST' action='editHealthContent.php'>
                 <button class='ui right floated mini button' onclick='/semantic/src/inc/editHealthContent.php'
                         type='submit'>
                     <input type='hidden' name='editHealth' value=$ healthContentID> Edit
                 </button>
             </form>
-        </div>
-        <div class = 'row'>
-            <div class='eight wide column'>
-                <h3 class='ui header' id='title'>
-                    $title
-                </h3>
-                <p id='datePosted'>$datePosted
-                    <br>
-                </p>
-                <div class='ui small image'>
-                    <img src='$photoURL'>
+            <div class='row'>
+                <div class='eight wide column'>
+                    <h3 class='ui header' id='title'>
+                        $title
+                    </h3>
+                    <p id='datePosted'>$datePosted
+                        <br>
+                    </p>
+                    <div class='ui small image'>
+                        <img src='$photoURL'>
+                    </div>
                 </div>
-            </div>
-            <div class='eight wide column'>
-                <p id='mainText' style='text - align:justify'> $mainText
-                    <br>
-                </p>
-                <p id='author'> By $userName
-                    <br>
-                </p>
+                <div class='eight wide column'>
+                    <p id='mainText' style='text - align:justify'> $mainText
+                        <br>
+                    </p>
+                    <p id='author'> By $userName
+                        <br>
+                    </p>
+                </div>
             </div>
         </div>
         <div class='ui hidden section divider'></div>
     </div>
 </div>
+
 ";
                         } else {
                             echo "
