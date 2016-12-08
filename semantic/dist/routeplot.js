4bb18e1a277fd9e6e2c8b942aa18a999e1b54f73
ross2106 <ross1994@hotmail.co.uk>
2016-12-08 23:07:08 +0000

    map page

--- semantic/dist/routeplot.js
+++ semantic/dist/routeplot.js
@@ -1,22 +1,28 @@
 function initMap() {
+    var latlng = {lat: 57.063171, lng: -2.139793};
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 12,
         center: {lat: 57.061681, lng: -2.129468}  // Portlethen.
     });
 
-    var directionsService = new google.maps.DirectionsService;
-    var directionsDisplay = new google.maps.DirectionsRenderer({
-        draggable: true,
+    // var directionsService = new google.maps.DirectionsService;
+    // var directionsDisplay = new google.maps.DirectionsRenderer({
+    //     draggable: true,
+    //     map: map,
+    //     panel: document.getElementById('right-panel')
+    // });
+    //
+    // directionsDisplay.addListener('directions_changed', function() {
+    //     computeTotalDistance(directionsDisplay.getDirections());
+    // });
+    //
+    // displayRoute('57.061959, -2.131337', '57.054993, -2.145402', directionsService,
+    //     directionsDisplay);
+    var marker = new google.maps.Marker({
+        position: myLatLng,
         map: map,
-        panel: document.getElementById('right-panel')
+        title: 'Hello World!'
     });
-
-    directionsDisplay.addListener('directions_changed', function() {
-        computeTotalDistance(directionsDisplay.getDirections());
-    });
-
-    displayRoute('57.061959, -2.131337', '57.054993, -2.145402', directionsService,
-        directionsDisplay);
 }
 
 function displayRoute(origin, destination, service, display) {
