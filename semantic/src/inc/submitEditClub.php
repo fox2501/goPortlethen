19a5c8d6997c1bbbd249c3c8abcd495f10f53182
ross2106 <ross1994@hotmail.co.uk>
2016-12-08 13:35:52 +0000

    Edit Club Page

--- semantic/src/inc/submitEditClub.php
+++ semantic/src/inc/submitEditClub.php
@@ -3,12 +3,12 @@ session_start();
 include("includes/dbconnect.php");
 
 $clubName = $_POST["editClubName"];
-$clubID = $_POST['clubToEdit'];
+$clubID = $_POST["clubToEdit"];
 $clubDesc = $_POST["editDescription"];
 $contactNum = $_POST["editNumber"];
-$websiteURL = $_POST['editURL'];
-$feeRequired = $_POST['editFeeRequired'];
-$feeCost = $_POST['editCost'];
+$websiteURL = $_POST["editURL"];
+$feeRequired = $_POST["editFeeRequired"];
+$feeCost = $_POST["editCost"];
 
 //FEE REQUIRED DOES NOT WORK
 
@@ -20,9 +20,20 @@ contactNumber = '$contactNum',
 websiteURL = '$websiteURL',
 feePaid = '0',
 feeCost = '$feeCost',
-WHERE clubID = $clubID;
+WHERE clubID = '$clubID';
 ";
 $result = mysqli_query($db, $sql);
 
-header('Location: /semantic/src/inc/clubLandingPage.php');
+if($result){
+    header('Location: /semantic/src/inc/clubLandingPage.php');
+} else{
+    echo $clubName."\r\n";
+    echo $clubID."\r\n";
+    echo $clubDesc."\r\n";
+    echo $contactNum."\r\n";
+    echo $websiteURL."\r\n";
+    echo $feeRequired."\r\n";
+    echo $feeCost."\r\n";
+}
+
 ?>
