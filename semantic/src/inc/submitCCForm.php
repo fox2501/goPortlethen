5c277ffbba165c30cd01f1b0b1333f6b068269dd
fox2501 <liamstewart777@hotmail.co.uk>
2016-12-08 21:15:44 +0000

    PDO to protect from sql injections

--- semantic/src/inc/submitCCForm.php
+++ semantic/src/inc/submitCCForm.php
@@ -1,69 +1,69 @@
 <?php
 session_start();
-include("includes/dbconnect.php");
-$userID = $_SESSION['loggedIn'];
-$clubName = $_POST["clubName"];
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+include("includes/PDOConnect.php");
+$userID          = $_SESSION['loggedIn'];
+$clubName        = $_POST["clubName"];
 $clubDescription = $_POST["clubDescription"];
-$email = $_POST["email"];
-$contactNumber = $_POST["phoneNumber"];
-//$calendarID = $_POST["calendarID"];
-$feePaid = $_POST["feeRequired"];
-if(empty($feePaid)){
+$email           = $_POST["email"];
+$contactNumber   = $_POST["phoneNumber"];
+$feePaid         = $_POST["feeRequired"];
+if (empty($feePaid)) {
     $feePaid = 0;
 }
 $feeCost = $_POST["feeAmount"];
-if(empty($feeCost)){
+if (empty($feeCost)) {
     $feeCost = 0;
 }
 $clubCategory = $_POST["clubCategory"];
 
 
-$img=$_FILES['img'];
-    if($img['name']==''){
-        echo "<h2>An Image Please.</h2>";
-    }else{
-        $filename = $img['tmp_name'];
-        $client_id="54dbcfe7d59ad73";
-        $handle = fopen($filename, "r");
-        $data = fread($handle, filesize($filename));
-        $pvars   = array('image' => base64_encode($data));
-        $timeout = 30;
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($curl, CURLOPT_URL, 'https://api.imgur.com/3/image.json');
-        curl_setopt($curl, CURLOPT_TIMEOUT, $timeout);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Client-ID ' . $client_id));
-        curl_setopt($curl, CURLOPT_POST, 1);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $pvars);
-        $out = curl_exec($curl);
-        curl_close ($curl);
-        $pms = json_decode($out,true);
-        $url=$pms['data']['link'];
-    }
-$sql = "INSERT INTO club (clubName, clubDescription, email, contactNumber, calendarID, feePaid, feeCost, url, clubCategory, timeStamp, userID)
-VALUES ('$clubName', '$clubDescription', '$email', '$contactNumber', '11', '$feePaid', '$feeCost', 'testurl','$clubCategory', CURRENT_TIMESTAMP, $userID)";
-$result = mysqli_query($db, $sql);
-
-if($result){
-    sleep(2);
-} else{
-    die;
+$img = $_FILES['img'];
+if ($img['name'] == '') {
+    echo "<h2>An Image Please.</h2>";
+} else {
+    $filename  = $img['tmp_name'];
+    $client_id = "54dbcfe7d59ad73";
+    $handle    = fopen($filename, "r");
+    $data      = fread($handle, filesize($filename));
+    $pvars     = array(
+        'image' => base64_encode($data)
+    );
+    $timeout   = 30;
+    $curl      = curl_init();
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_URL, 'https://api.imgur.com/3/image.json');
+    curl_setopt($curl, CURLOPT_TIMEOUT, $timeout);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+        'Authorization: Client-ID ' . $client_id
+    ));
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $pvars);
+    $out = curl_exec($curl);
+    curl_close($curl);
+    $pms = json_decode($out, true);
+    $url = $pms['data']['link'];
 }
 
-if(mysqli_affected_rows($db) > 0){
-    $sql2 ="SELECT clubID FROM club WHERE timeStamp = (SELECT MAX(timeStamp) FROM club)";
-    $result = mysqli_query($db, $sql2);
-    while($row = mysqli_fetch_assoc($result)){
-        $clubID = $row['clubID'];
-    }
+$sql    = "INSERT INTO club (clubName, clubDescription, email, contactNumber, calendarID, feePaid, feeCost, url, clubCategory, userID)
+VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+$pdo->prepare($sql)->execute([$clubName,$clubDescription,$email,$contactNumber,0,$feePaid,$feeCost,$url,$clubCategory,$userID]);
 
-    $sql1 = "INSERT INTO photos (caption,url,clubID,locationID,healthContentID,routeID) VALUES ('test','$url',$clubID,'0','0','0')";
+usleep(10000);
 
-    $result = (mysqli_query($db, $sql1));
+$clubID = $pdo->lastInsertId();
+
+$sql1 = "INSERT INTO photos (caption,url,clubID,locationID,healthContentID,routeID) VALUES (?,?,?,?,?,?)";
+
+$pdo->prepare($sql1)->execute(['',$url,$clubID,0,0,0]);
+
 
     header("location:clubLandingPage.php");
-} else{
-    echo "Create club failed.";
-}
+
 ?>
