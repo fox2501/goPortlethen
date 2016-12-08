272b501f4783750ddc355806d4c27eb92260fc1a
Liam Stewart <liamstewart777@hotmail.co.uk>
2016-12-08 14:23:38 +0000

    block SQL injections

--- semantic/src/inc/submitEditHealth.php
+++ semantic/src/inc/submitEditHealth.php
@@ -1,19 +1,23 @@
 <?php
 session_start();
-include("includes/dbconnect.php");
+include("includes/PDOConnect.php.php");
 
 $title = $_POST["editTitle"];
 $mainText = $_POST["editMainText"];
 $healthContentID = $_POST["healthContentToEdit"];
 $userID = $_SESSION['loggedIn'];
 
-$sql = "
-UPDATE healthcontent
-SET title = '$title',
-mainText = '$mainText',
-WHERE healthContentID = $healthContentID;
-";
-$result = mysqli_query($db, $sql);
+$sql ="UPDATE healtcontent SET title = ?,mainText= ? WHERE healthContentID = ?";
+$pdo->prepare($sql)->execute([$title,$mainText,$healthContentID]);
+
+
+//$sql = "
+//UPDATE healthcontent
+//SET title = '$title',
+//mainText = '$mainText',
+//WHERE healthContentID = $healthContentID;
+//";
+//$result = mysqli_query($db, $sql);
 
 header('Location: /semantic/src/inc/health.php');
 ?>
