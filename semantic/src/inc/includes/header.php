8844ab03f78e690e053dba3c1c0d1bd233e72a8e
ross2106 <ross1994@hotmail.co.uk>
2016-12-08 21:46:10 +0000

    health approvals

--- semantic/src/inc/includes/header.php
+++ semantic/src/inc/includes/header.php
@@ -21,6 +21,7 @@ include("dbconnect.php");
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="/semantic/dist/semantic.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
