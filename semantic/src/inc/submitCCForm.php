46e40fd6a6e582161a17eced42f563e2cb166440
ross2106 <ross1994@hotmail.co.uk>
2016-12-08 11:37:12 +0000

    Edit health page

--- semantic/src/inc/submitCCForm.php
+++ semantic/src/inc/submitCCForm.php
@@ -38,6 +38,8 @@ $sql = "INSERT INTO club (clubName, clubDescription, email, contactNumber, calen
 VALUES ('$clubName', '$clubDescription', '$email', '$contactNumber', '11', '$feePaid', '$feeCost', 'testurl','$clubCategory', CURRENT_TIMESTAMP)";
 $result = mysqli_query($db, $sql);
 
+sleep(2);
+
 if(mysqli_affected_rows() > 0){
     $sql2 ="SELECT clubID FROM club WHERE timeStamp = (SELECT MAX(timeStamp) FROM club)";
     $result = mysqli_query($db, $sql2);
