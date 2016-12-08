8331a3e600dedbff08dfb6b7f0c8867fdf355530
fox2501 <liamstewart777@hotmail.co.uk>
2016-12-08 19:30:45 +0000

    fixsubmitupload

--- semantic/src/inc/healthForm.php
+++ semantic/src/inc/healthForm.php
@@ -3,7 +3,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 session_start();
-include("includes/dbconnect.php");
 include("includes/PDOConnect.php");
 if (isset($_SESSION['loggedIn'])) {
     $userID = $_SESSION['loggedIn'];
