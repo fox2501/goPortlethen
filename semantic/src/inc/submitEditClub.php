74596d69f123754e9e57e1cfa72e97e98b08c814
ross2106 <ross1994@hotmail.co.uk>
2016-12-08 13:25:37 +0000

    Edit Club Page

--- semantic/src/inc/submitEditClub.php
+++ semantic/src/inc/submitEditClub.php
@@ -10,17 +10,25 @@ $websiteURL = $_POST['editURL'];
 $feeRequired = $_POST['editFeeRequired'];
 $feeCost = $_POST['editCost'];
 
-$sql = "
-UPDATE club
-SET clubName = '$clubName',
-clubDescription = '$clubDesc',
-contactNumber = '$contactNum',
-websiteURL = '$websiteURL',
-feePaid = '$feeRequired',
-feeCost = '$feeCost',
-WHERE clubID = $clubID;
-";
-$result = mysqli_query($db, $sql);
+echo $clubName;
+echo $clubID;
+echo $clubDesc;
+echo $contactNum;
+echo $websiteURL;
+echo $feeRequired;
+echo $feeCost;
 
-header('Location: /semantic/src/inc/clubLandingPage.php');
+//$sql = "
+//UPDATE club
+//SET clubName = '$clubName',
+//clubDescription = '$clubDesc',
+//contactNumber = '$contactNum',
+//websiteURL = '$websiteURL',
+//feePaid = '$feeRequired',
+//feeCost = '$feeCost',
+//WHERE clubID = $clubID;
+//";
+//$result = mysqli_query($db, $sql);
+//
+//header('Location: /semantic/src/inc/clubLandingPage.php');
 ?>
