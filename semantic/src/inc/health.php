2f094a5e1e815c83772dca34bd0ba013f1505f6f
Liam Stewart <liamstewart777@hotmail.co.uk>
2016-12-08 12:04:25 +0000

    photo upload club

--- semantic/src/inc/health.php
+++ semantic/src/inc/health.php
@@ -157,6 +157,7 @@ AND A.healthContentID = C.healthContentID";
         <div class="six wide column">
             <div data-tockify-component="mini" data-tockify-calendar="healthevents"></div>
             <script data-tockify-script="embed" src="https://public.tockify.com/browser/embed.js"></script>
+            <button class="ui button"><a href="https://tockify.com/tkf2/submitEvent/42648d506ec74f769ce92685c0fe921e" target="_blank">Submit an Event</a></button>
             <div style="height:600px">
                 <a class="twitter-timeline" data-height="500" href="https://twitter.com/BoringMilner">Tweets by James Milner</a>
                 <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
