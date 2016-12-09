2f1e3b8f6504f3af7cf1c353dc8b7ddc1723e47c
ross2106 <ross1994@hotmail.co.uk>
2016-12-09 15:04:42 +0000

    javascript

--- semantic/src/inc/signUpForm.php
+++ semantic/src/inc/signUpForm.php
@@ -7,11 +7,13 @@ include("includes/dbconnect.php");
 <head>
     <meta charset="UTF-8">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js">
-    </script>
-    <script src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.js">
     </script>
+    <script src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js">
+    </script>
+
+
     <title>goPortlethen</title>
 </head><?php include("includes/header.php"); ?><?php $url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 ?>
