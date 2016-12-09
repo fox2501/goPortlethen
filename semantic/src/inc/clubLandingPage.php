0c78ad9af0243868e30079a2457c3b93d604c61c
Cameron Lawrie <cameron.lawrie1@gmail.com>
2016-12-09 13:19:11 +0000

    checkbox added

--- semantic/src/inc/clubLandingPage.php
+++ semantic/src/inc/clubLandingPage.php
@@ -47,13 +47,13 @@ include("includes/header.php");
                     <div class="inline fields">
                         <label>Filter clubs by: </label>
                         <div class="field">
-                            <div class="ui radio checkbox">
-                                <input type="radio" name="Club" checked="" tabindex="0" class="hidden" id="A-Z">
+                            <div class="ui checkbox">
+                                <input type="checkbox" tabindex="0" class="hidden" name="A-Z">
                                 <label for="A-Z">Name A-Z</label>
                             </div>
                         </div>
-                        <div class="ui radio checkbox">
-                            <input type="radio" name="Club" tabindex="0" class="hidden" id="fee">
+                        <div class="ui checkbox">
+                            <input type="checkbox" tabindex="0" class="hidden" name="fee">
                             <label for="fee">Fee does apply</label>
                         </div>
                     </div>
