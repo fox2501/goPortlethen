1af066d69e661dc364ff63f0efbde2db9c9a88d4
Cameron Lawrie <cameron.lawrie1@gmail.com>
2016-12-09 13:55:44 +0000

    change toggle to checkbox

--- semantic/src/inc/createClubPage.php
+++ semantic/src/inc/createClubPage.php
@@ -88,10 +88,9 @@ if (isset($_SESSION['loggedIn'])) {
                     <input type="text" name="websiteURL" placeholder="Website URL">
                 </div>
                 <div class="field">
-                    <label>Please check if your club requires a fee: </label>
-                    <div class="ui checkbox">
-                        <input type="checkbox" name="isFee" tabindex="0" class="hidden" id="isFee" value="0">
-                        <input type="text" name="feeRequired" placeholder="Amount" id="feeRequired">
+                    <label>Please toggle if your club requires a fee: </label>
+                    <div class="ui toggle checkbox">
+                        <input type="checkbox" name="feeRequired" tabindex="0" class="hidden" id="isFee" value="0">
                         <label for="isFee">Fee does apply</label>
                     </div>
                 </div>
