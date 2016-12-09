5e3a87894fc7d651890c0135c94d44bc0d382858
ross2106 <ross1994@hotmail.co.uk>
2016-12-09 00:14:57 +0000

    map page

--- semantic/src/inc/mapPage.php
+++ semantic/src/inc/mapPage.php
@@ -13,6 +13,8 @@ while($row = mysqli_fetch_assoc($result)){
 }
 include("includes/header.php");
 ?>
+<!DOCTYPE html>
+<html>
 <head>
 
     <script src="https://goportlethencs8.azurewebsites.net/semantic/dist/routeplot.js" type="text/javascript">
@@ -28,20 +30,23 @@ include("includes/header.php");
 <div class="ui horizontal section divider">
     Discover Portlethen
 </div>
-<div class="ui equal width grid">
-    <header class = 'ui header'><?php echo $locationName ?></header>
-    <div class="twelve wide column">
+<div class="ui container">
+    <header class = 'ui header'>
+        <?php echo $locationName ?>
+    </header>
+    <div class = 'ui grid'>
+    <div class="sixteen wide column">
         <div id="map" style="width: 600px; height: 350px"></div>
         <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAAsaPQGyO2SHJumHMC2k8RTYfy3z7OXIk&callback=initMap">
         </script>
     </div>
 </div>
 <div class="ui grid">
+    <div class="four wide column"></div>
     <div class="eight wide column">
         <h3>Description</h3>
         <p><?php echo $caption ?></p>
-        <p>On its way the Trail passes peaceful water meadows rich in wildlife, historic towns and cities and many lovely villages, finishing at the edinburgh town just a few miles from the sea.</p>
     </div>
-</div>
-<?php include("includes/footer.php"); ?>
+</div><?php include("includes/footer.php"); ?>
 </body>
+</html>
