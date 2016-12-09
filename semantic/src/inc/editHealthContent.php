daba377b0c11417d7315caf92e2b07b90c139b1d
ross2106 <ross1994@hotmail.co.uk>
2016-12-09 14:16:21 +0000

    map page

--- semantic/src/inc/editHealthContent.php
+++ semantic/src/inc/editHealthContent.php
@@ -2,89 +2,86 @@
 session_start();
 include("includes/dbconnect.php");
 if (isset($_SESSION['loggedIn'])) {
-    $userID = $_SESSION['loggedIn'];
-    $healthContentID = $_POST['editHealth'];
-    $sql = "SELECT A.accessID from useraccess A, users B WHERE A.userName = B.userName AND B.userID = '$userID'";
-    $result = mysqli_query($db, $sql);
-    while($row = mysqli_fetch_assoc($result)){
-        $accessLevel = $row['accessID'];
-    }
-    $sql = "SELECT userID from healthcontent where healthContentID = $healthContentID";
-    $result = mysqli_query($db, $sql);
-    while ($row = mysqli_fetch_assoc($result)) {
-        if ($row['userID'] == $userID || $accessLevel == '1') {
-            $sql = "SELECT * from healthcontent WHERE healthContentID = $healthContentID";
-            $result = mysqli_query($db, $sql);
-            while ($row = mysqli_fetch_assoc($result)) {
-                $title = $row['title'];
-                $mainText = $row['mainText'];
-                $healthContentID = $row['healthContentID'];
-            }
-            ?>
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <link rel="stylesheet" type="text/css" href="semantic.css">
-                <title>Semantic UI</title>
-            </head>
-
-            <!-- Nav bar -->
-            <? include("includes/header.php"); ?>
-
-            <body>
-
-            <!-- Form -->
-            <div class="ui container">
-                <header class="ui huge blue header">Edit Health & Wellbeing Content</header>
-                <form class="ui form" action="submitEditHealth.php" enctype="multipart/form-data" method="POST">
-                    <div class="field">
-                        <label>Edit Title</label>
-                        <input type="text" name="editTitle" value=
-                        "<?php
-                        echo $title;
-                        ?>">
-                    </div>
-                    <div class="field">
-                        <label>Edit Main Text</label>
-                        <textarea rows=8 type="text" name="editMainText"><?php echo $mainText; ?></textarea>
-                    </div>
-                    <div class="field">
-                        <input type="hidden" name="healthContentToEdit" value=
-                        "<?php echo $healthContentID ?>"/>
-                    </div>
-                    <div class="field">
-                        <label>Choose a new image for the content</label>
-                        <div class="ui fluid action input">
-                            <input name="healthPhoto" size="35" type="file"/>
-                        </div>
-                    </div>
-                    <button class="ui fluid large green submit button" type="submit">Submit</button>
-                </form>
+$userID = $_SESSION['loggedIn'];
+$sql = "SELECT A.accessID from useraccess A, users B WHERE A.userName = B.userName AND B.userID = '$userID'";
+$result = mysqli_query($db, $sql);
+while($row = mysqli_fetch_assoc($result)){
+    $accessLevel = $row['accessID'];
+}
+$healthContentID = $_POST['editHealth'];
+$sql = "SELECT userID from healthcontent where healthContentID = $healthContentID";
+$result = mysqli_query($db, $sql);
+while ($row = mysqli_fetch_assoc($result)) {
+    $healthUserID = $row['userID'];
+}
+if($healthUserID == $userID){
+    $canAccess = 1;
+}
+if($accessLevel == 1){
+    $canAccess = 1;
+}
+if ($canAccess = 1) {
+$sql = "SELECT * from healthcontent WHERE healthContentID = $healthContentID";
+$result = mysqli_query($db, $sql);
+while ($row = mysqli_fetch_assoc($result)) {
+    $title = $row['title'];
+    $mainText = $row['mainText'];
+    $healthContentID = $row['healthContentID'];
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="semantic.css" rel="stylesheet" type="text/css">
+    <title>Semantic UI</title>
+</head><!-- Nav bar -->
+<? include("includes/header.php"); ?>
+<body>
+<!-- Form -->
+<div class="ui container">
+    <header class="ui huge blue header">
+        Edit Health & Wellbeing Content
+    </header>
+    <form action="submitEditHealth.php" class="ui form" enctype="multipart/form-data" method="post">
+        <div class="field">
+            <label>Edit Title</label> <input name="editTitle" type="text" value="<?php echo $title; ?>">
+        </div>
+        <div class="field">
+            <label>Edit Main Text</label>
+            <textarea name="editMainText" rows="8"><?php echo $mainText; ?></textarea>
+        </div>
+        <div class="field">
+            <input name="healthContentToEdit" type="hidden" value="<?php echo $healthContentID ?>">
+        </div>
+        <div class="field">
+            <label>Choose a new image for the content</label>
+            <div class="ui fluid action input">
+                <input name="healthPhoto" size="35" type="file">
             </div>
+        </div><button class="ui fluid large green submit button" type="submit">Submit</button>
+    </form>
+</div>
+<script type="text/javascript">
+    function validateForm() {
+        var x = document.forms["healthForm"] ["title"].value;
+        var y = document.forms["healthForm"] ["mainText"].value;
 
-            <script type="text/javascript">
-                function validateForm() {
-                    var x = document.forms["healthForm"] ["title"].value;
-                    var y = document.forms["healthForm"] ["mainText"].value;
+        if (x == "") {
+            alert("Please enter a title.")
+        }
 
-                    if (x == "") {
-                        alert("Please enter a title.")
-                    }
-
-                    else if (y == "") {
-                        alert("Please enter some content.")
-                    }
-                }
-            </script>
-            </body>
-            </html>
-            <!-- Footer -->
-            <?php
-            include("includes/footer.php");
-        } else {
-            echo "You do not have permission.";
+        else if (y == "") {
+            alert("Please enter some content.")
         }
     }
+</script><!-- Footer -->
+<?php
+include("includes/footer.php");
+} else {
+    echo "You do not have permission.";
+}
 };
 ?>
+</body>
+</html>
