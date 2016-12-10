18bd7ad0df55b70861fac1be9485bf6ddf6709ad
ross2106 <ross1994@hotmail.co.uk>
2016-12-10 11:26:30 +0000

    javascript

--- semantic/src/inc/deleteProfile.php
+++ semantic/src/inc/deleteProfile.php
@@ -12,15 +12,13 @@ include("includes/header.php");
 <body>
 <div class='ui center aligned container'>
     <div class='ui middle aligned center aligned grid'>
-        <div class = 'four wide column'></div>
-        <div class='ui column'>
+        <div class='ui ten wide column'>
             <div class="ui large form">
                 <div class='ui negative message'>
                     <h1 class='header'>ARE YOU SURE YOU WANT TO DELETE YOUR PROFILE?</h1>
                 </div>
             </div><button class="ui fluid large red submit button" type="submit">Delete Profile</button>
         </div>
-        <div class = 'four wide column'></div>
     </div>
 </div><?php
 include("includes/footer.php");
