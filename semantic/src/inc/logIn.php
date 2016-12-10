fa8db2cf8e25ce71aaa25bd253afe3d51601a08c
ross2106 <ross1994@hotmail.co.uk>
2016-12-10 11:14:08 +0000

    javascript

--- semantic/src/inc/logIn.php
+++ semantic/src/inc/logIn.php
@@ -4,7 +4,6 @@ session_start();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
     <title>goPortlethen</title>
 </head><?php
 $url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
