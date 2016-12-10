3966c79940ae1912254cef6f546660e6f384df3d
ross2106 <ross1994@hotmail.co.uk>
2016-12-10 11:23:19 +0000

    javascript

---
+++ semantic/src/inc/deleteProfile.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+include("includes/dbconnect.php");
+if(isset($_SESSION['loggedIn'])){
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>goPortlethen</title>
+</head>
+<body>
+<div class='ui container'>
+    <div class='ui grid'>
+        <div class='ui column'>
+            <div class="ui large form">
+                <div class='ui negative message'>
+                    <h1 class='header'>ARE YOU SURE YOU WANT TO DELETE YOUR PROFILE?</h1>
+                </div>
+            </div><button class="ui fluid large red submit button" type="submit">Delete Profile</button>
+        </div>
+    </div>
+</div><?php
+}
+?>
+</body>
+</html>
