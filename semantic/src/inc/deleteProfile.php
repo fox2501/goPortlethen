1baf356c17ceaf5986fa103fdbb2cec0c52f4a1e
ross2106 <ross1994@hotmail.co.uk>
2016-12-10 11:29:25 +0000

    javascript

--- semantic/src/inc/deleteProfile.php
+++ semantic/src/inc/deleteProfile.php
@@ -11,10 +11,6 @@ include("includes/header.php");
 </head>
 <body>
 <div class='ui center aligned container'>
-    <h2 class="ui blue header"></h2>
-    <div class="content">
-        <h2 class="ui blue header">Delete your account</h2>
-    </div>
     <div class='ui middle aligned center aligned grid'>
         <div class='ui ten wide column'>
             <div class="ui large form">
