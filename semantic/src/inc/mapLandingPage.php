1d36602f9132bded0958e4ef37d7c46765b594dc
ross2106 <ross1994@hotmail.co.uk>
2016-12-11 23:08:36 +0000

    Edit Map

--- semantic/src/inc/mapLandingPage.php
+++ semantic/src/inc/mapLandingPage.php
@@ -11,7 +11,7 @@ if (isset($_SESSION['loggedIn'])) {
     $row = $stmt -> fetch(PDO::FETCH_ASSOC);
     $accessID = $row["accessID"];
 
-    if ($accessID == '1' || $accessID == '4') {
+    if ($accessID == '1' || $accessID == '3') {
         $canAccess = '1';
     } else {
         $canAccess = '0';
