1e69069a23e13666fbfed4c57846101cd9c386d0
ross2106 <ross1994@hotmail.co.uk>
2016-12-11 23:09:41 +0000

    Edit Map

--- semantic/src/inc/mapPage.php
+++ semantic/src/inc/mapPage.php
@@ -63,7 +63,7 @@ include("includes/header.php");
         if($canAccess == 1){
             echo "        <div class='two wide column'>
             <form action='editMap.php' class='ui form' method='post'>
-                <button class='ui right floated button' onclick='/semantic/src/inc/editMap.php' type='submit'><input name='editMap' type='hidden' value='<?php echo $locationID ?>'> Edit</button>
+                <button class='ui right floated button' onclick='/semantic/src/inc/editMap.php' type='submit'><input name='editMap' type='hidden' value='$locationID'> Edit</button>
             </form>
         </div>";
         }
