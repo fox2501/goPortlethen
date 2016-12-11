777991520d1f18dd7372d45e1eb8fc2b35df90ef
ross2106 <ross1994@hotmail.co.uk>
2016-12-11 14:07:51 +0000

    Delete profile

--- semantic/src/inc/submitDeleteProfile.php
+++ semantic/src/inc/submitDeleteProfile.php
@@ -16,24 +16,26 @@ $stmt->execute([$userID]);
 
 while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
     $userAccessID = $row['userAccessID'];
+    $sql = "DELETE FROM useraccess WHERE userAccessID = '?'";
+    $stmt = $pdo->prepare($sql);
+    $stmt -> execute([$userAccessID]);
+
+    $sql = "DELETE FROM users WHERE userID = '?'";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$userID]);
+
+
 }
+header('Location: /semantic/');
 //$result = mysqli_query($db, $sql);
 //while($row = $result ->fetch_assoc()){
 //    $userAccessID = $row['userAccessID'];
 //}
-echo $userAccessID;
-echo $userID;
+//echo $userAccessID;
+//echo $userID;
 
 
-//$sql = "DELETE FROM useraccess WHERE userAccessID = '?'";
-//$stmt = $pdo->prepare($sql);
-//$stmt -> execute([$userAccessID]);
-//
-//    $sql = "DELETE FROM users WHERE userID = '?'";
-//    $stmt = $pdo->prepare($sql);
-//    $stmt->execute([$userID]);
-//
-//    header('Location: /semantic/');
+
 
 
 ?>
