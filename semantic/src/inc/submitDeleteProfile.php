a30e764a8ca47c79f4e06a1d708a29896c724b37
ross2106 <ross1994@hotmail.co.uk>
2016-12-11 13:53:59 +0000

    Delete profile

--- semantic/src/inc/submitDeleteProfile.php
+++ semantic/src/inc/submitDeleteProfile.php
@@ -10,34 +10,31 @@ $userID = $_SESSION['loggedIn'];
 
 //FEE REQUIRED DOES NOT WORK
 
-$sql = "SELECT userAccessID from useraccess A, users B WHERE A.userName = B.userName AND B.userID = '?'";
-$stmt = $pdo->prepare($sql)->execute([$userID]);
+$sql = "SELECT userAccessID from useraccess A, users B WHERE A.userName = B.userName AND B.userID = $userID";
+$stmt = $pdo->prepare($sql)->execute($userID);
+
+while($row = $stmt->fetch()){
+    $userAccessID = $row['userAccessID'];
+}
 //$result = mysqli_query($db, $sql);
 //while($row = $result ->fetch_assoc()){
 //    $userAccessID = $row['userAccessID'];
 //}
-while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-    $userAccessID = $row['userAccessID'];
+
+
+$sql = "DELETE FROM useraccess WHERE userAccessID = '?'";
+$execute = $pdo->prepare($sql)->execute($userAccessID);
+
+if ($execute)
+{
+    $sql = "DELETE FROM users WHERE userID = '?'";
+    $pdo->prepare($sql)->execute($userID);
+
+    header('Location: /semantic/');
+} else{
+    header('Location: /semantic/src/inc/deleteProfile.php');
+    exit();
 }
-
-//$result = $stmt->fetch(PDO::FETCH_ASSOC);
-
-//echo $userID."<br>";
-echo $userAccessID;
-
-//$sql = "DELETE FROM useraccess WHERE userAccessID = '?'";
-//$execute = $pdo->prepare($sql)->execute($userAccessID);
-//
-//if ($execute)
-//{
-//    $sql = "DELETE FROM users WHERE userID = '?'";
-//    $pdo->prepare($sql)->execute($userID);
-//
-//    header('Location: /semantic/');
-//} else{
-//    header('Location: /semantic/src/inc/deleteProfile.php');
-//    exit();
-//}
 
 
 ?>
