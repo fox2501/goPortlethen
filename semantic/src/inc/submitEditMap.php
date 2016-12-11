ea23cc2f0f88a7bd67bfc1617a8af33c0c265c6e
ross2106 <ross1994@hotmail.co.uk>
2016-12-11 22:19:49 +0000

    Edit Map

--- semantic/src/inc/submitEditMap.php
+++ semantic/src/inc/submitEditMap.php
@@ -12,7 +12,7 @@ $lat = $_POST['lat'];
 $lng = $_POST['long'];
 $mapDesc = $_POST['mapDesc'];
 
-$sql = "UPDATE locations SET locationName = ?, mapType = ?, latitude = ?, longitude = ?, caption = ? WHERE locationID = ?";
+$sql = "UPDATE locations SET locationName = ?, locationType = ?, latitude = ?, longitude = ?, caption = ? WHERE locationID = ?";
 $stmt = $pdo -> prepare($sql);
 $stmt -> execute([$title, $mapType, $lat, $lng, $mapDesc, $locationID]);
 
