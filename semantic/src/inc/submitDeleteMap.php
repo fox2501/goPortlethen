3408f5b69a3573a144c1433574b24c83d4d0ecde
ross2106 <ross1994@hotmail.co.uk>
2016-12-11 23:25:22 +0000

    Edit Map

--- semantic/src/inc/submitDeleteMap.php
+++ semantic/src/inc/submitDeleteMap.php
@@ -7,6 +7,6 @@ $locationID =$_POST["deleteMap"];
 $sql = "DELETE FROM locations WHERE locationID = ?";
 $stmt = $pdo->prepare($sql)->execute([$locationID]);
 
-header('Location: /semantic/src/inc/mapLandingPage.php');
+header('Location: /semantic/src/inc/mapLandingPage.php?DeletedMap');
 
 ?>
