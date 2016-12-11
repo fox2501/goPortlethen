c3ae9592bc6cb067066b0ab32bb18668dcc19bbc
fox2501 <liamstewart777@hotmail.co.uk>
2016-12-11 16:45:31 +0000

    delete profile

--- semantic/src/inc/picUp.php
+++ semantic/src/inc/picUp.php
@@ -7,9 +7,9 @@ include_once('functions.php');
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>PHP Event Calendar by CodexWorld</title>
     <!-- Include the stylesheet -->
-    <link type="text/css" rel="stylesheet" href="style.css"/>
+    <link type="text/css" rel="stylesheet" href="semantic/dist/semantic.css"/>
     <!-- Include the jQuery library -->
-    <script src="jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 </head>
 <body>
 <!-- Display event calendar -->
