69f4ba68fb6b4cd6efbc85035dcfd8a7793cd705
Lorna <lornam@me.com>
2016-12-11 19:42:57 +0000

    health page green event button

--- semantic/src/inc/health.php
+++ semantic/src/inc/health.php
@@ -131,7 +131,7 @@ if (isset($_SESSION['loggedIn'])) {
             <div data-tockify-component="mini" data-tockify-calendar="healthevents"></div>
             <script data-tockify-script="embed" src="https://public.tockify.com/browser/embed.js"></script>
             <button class="ui green submit button"><a href="https://tockify.com/tkf2/submitEvent/42648d506ec74f769ce92685c0fe921e"
-                                         target="_blank">Submit an Event</a></button>
+                                         target="_blank" style="color: white">Submit an Event</a></button>
             <div style="height:600px">
                 <a class="twitter-timeline" data-height="500" href="https://twitter.com/BoringMilner">Tweets by James
                     Milner</a>
