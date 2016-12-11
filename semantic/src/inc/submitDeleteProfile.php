c3502877b66fa3e7070931396aed3fbcbfdbd1af
ross2106 <ross1994@hotmail.co.uk>
2016-12-11 13:58:37 +0000

    Delete profile

--- semantic/src/inc/submitDeleteProfile.php
+++ semantic/src/inc/submitDeleteProfile.php
@@ -11,7 +11,8 @@ $userID = $_SESSION['loggedIn'];
 //FEE REQUIRED DOES NOT WORK
 
 $sql = "SELECT userAccessID from useraccess A, users B WHERE A.userName = B.userName AND B.userID = '?'";
-$stmt = $pdo->prepare($sql)->execute([$userID]);
+$stmt = $pdo->prepare($sql);
+$stmt->execute([$userID]);
 
 while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
     $userAccessID = $row['userAccessID'];
