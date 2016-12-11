7dd5ab1f747be1c91cc1d1e7596786252cd669e2
Lorna <lornam@me.com>
2016-12-11 19:37:41 +0000

    health page green event button

--- semantic/src/inc/health.php
+++ semantic/src/inc/health.php
@@ -136,6 +136,7 @@ if (isset($_SESSION['loggedIn'])) {
                 <a class="twitter-timeline" data-height="500" href="https://twitter.com/BoringMilner">Tweets by James
                     Milner</a>
                 <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
             </div>
         </div>
     </div>
