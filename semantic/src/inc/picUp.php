5879093ea0019a39b3ad047ee6e04c87c0a84e0a
fox2501 <liamstewart777@hotmail.co.uk>
2016-12-11 16:41:49 +0000

    test calendar

--- semantic/src/inc/picUp.php
+++ semantic/src/inc/picUp.php
@@ -1,27 +1,20 @@
-
-<form action="testPictureUpload.php" enctype="multipart/form-data" method="POST">
-    Choose Image : <input name="img" size="35" type="file"/><br/>
-    <input type="submit" name="submit" value="Upload"/>
-</form>
-
-<?php
-//
-//// Script to test if the CURL extension is installed on this server
-//
-//// Define function to test
-//function _is_curl_installed() {
-//    if  (in_array  ('curl', get_loaded_extensions())) {
-//        return true;
-//    }
-//    else {
-//        return false;
-//    }
-//}
-//
-//// Ouput text to user based on test
-//if (_is_curl_installed()) {
-//    echo "cURL is <span style=\"color:blue\">installed</span> on this server";
-//} else {
-//    echo "cURL is NOT <span style=\"color:red\">installed</span> on this server";
-//}
-//?>
+//Include the event calendar functions file
+include_once('functions.php');
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>PHP Event Calendar by CodexWorld</title>
+    <!-- Include the stylesheet -->
+    <link type="text/css" rel="stylesheet" href="style.css"/>
+    <!-- Include the jQuery library -->
+    <script src="jquery.min.js"></script>
+</head>
+<body>
+<!-- Display event calendar -->
+<div id="calendar_div">
+    <?php echo getCalender(); ?>
+</div>
+</body>
+</html>
