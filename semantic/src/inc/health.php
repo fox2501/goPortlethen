d0343637a651f7a2fee7e77f376be6da5e1f7827
ross2106 <ross1994@hotmail.co.uk>
2016-12-12 16:36:23 +0000

    Club Pages

--- semantic/src/inc/health.php
+++ semantic/src/inc/health.php
@@ -1,6 +1,10 @@
 <!-- Health and wellbeing landing page -->
 
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 //connects to database server
 include("includes/PDOConnect.php");
