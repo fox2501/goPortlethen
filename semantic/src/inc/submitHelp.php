4a98418709e0535d89096d084c6bca20d9e77e16
Alexander <xander19@btinternet.com>
2016-12-12 12:15:09 +0000

    email sending

--- semantic/src/inc/submitHelp.php
+++ semantic/src/inc/submitHelp.php
@@ -1,7 +1,10 @@
 <?php
 $firstname = $_POST['firstName'];
 $lastname = $_POST['lastName'];
-$emailID = $_POST['emailID'];
+$emailAddress = $_POST['emailAddress'];
 $question = $_POST['question'];
 $body = "From: $firstName\n E-Mail: $email\n Message:\n $message";
+
+SELECT emailAddress FROM users A, useraccess B WHERE A.userName = B.userName AND b.accessID = 1
+
 ?>
