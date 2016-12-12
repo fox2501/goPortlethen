3548ef73121d35433ded306998f90c1aec5ba649
Lorna <lornam@me.com>
2016-12-12 14:52:35 +0000

    club search

--- semantic/src/inc/submitClubSearch.php
+++ semantic/src/inc/submitClubSearch.php
@@ -4,11 +4,12 @@ include("includes/PDOConnect.php");
 
 $clubCategory = $_POST['clubCategory'];
 
-$sql = "SELECT * FROM clubs WHERE clubCategory = ?";
 $stmt = $pdo->prepare($sql);
 $stmt->execute([$clubCategory]);
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
 $clubCategory = $row["clubCategory"];
+$sql = "SELECT * FROM clubs WHERE clubCategory = ?";
+
 
 
 header ("Location: /semantic/src/inc/clubLandingPageSearched.php");
