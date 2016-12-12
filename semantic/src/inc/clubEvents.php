3c974d637ddda55c022fac802fd598b4d8145881
Liam Stewart <liamstewart777@hotmail.co.uk>
2016-12-12 21:14:36 +0000

    create club Events

--- semantic/src/inc/clubEvents.php
+++ semantic/src/inc/clubEvents.php
@@ -66,6 +66,7 @@ $clubName = $row['clubName'];
                                 <input type='hidden' name='createClubEvent' value=$clubID>
                                 <button class='ui green submit button' style='margin-right:50px'>Create Club Event</button>
                             </form>
+                            
                                 
                             </div>";
                 }
@@ -78,6 +79,7 @@ $clubName = $row['clubName'];
                     $title = $row['title'];
                     $created = $row['date'];
                     $description = $row['description'];
+                    $eventID = $row['id'];
                         echo "
             <div class='ui raised segment'>
                 <div class='ui stackable container'>
@@ -94,6 +96,12 @@ $clubName = $row['clubName'];
                             <form action='editClubEvent.php' class='ui form' method='post'>
                                 <button class='ui right floated mini button' onclick='/semantic/src/inc/editClubEvent.php' type='submit'><input name='editClub' type='hidden' value='$clubID'>Edit</button> 
                             </form>
+                            </div>
+                            <div class='two wide column'>
+                            <form action='deleteClubEvent.php' method='post'>
+                                <button class='ui red right floated button' name='eventID' type='submit' value='$eventID'>Delete Club</button>
+                            </form>
+                            </div>
                             ";}
                             echo "
                         </div>
