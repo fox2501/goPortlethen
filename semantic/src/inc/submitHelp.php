e8dbf53d12012b2a54777999a27639ed30622f35
ross2106 <ross1994@hotmail.co.uk>
2016-12-12 15:30:58 +0000

    Club Pages

--- semantic/src/inc/submitHelp.php
+++ semantic/src/inc/submitHelp.php
@@ -2,6 +2,9 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+include("includes/PDOConnect.php");
+
 $firstname = htmlentities($_POST['firstName']);
 $lastname = htmlentities($_POST['lastName']);
 $emailAddress = htmlentities($_POST['emailAddress']);
