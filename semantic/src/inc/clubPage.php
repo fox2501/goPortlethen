47c66a73cffdee487c9ffa69ebe71a8159918326
ross2106 <ross1994@hotmail.co.uk>
2016-12-12 01:25:32 +0000

    Merge branch 'master' of https://github.com/fox2501/goPortlethen
    # Conflicts:
    #	semantic/src/inc/clubPage.php

--- semantic/src/inc/clubPage.php
+++ semantic/src/inc/clubPage.php
@@ -3,11 +3,11 @@ session_start();
 include("includes/PDOConnect.php");
 
 $userID = $_SESSION['loggedIn'];
-$sql = "SELECT userAccessID from useraccess A, users B WHERE A.userName = B.userName AND B.userID = ?";
+$sql = "SELECT A.accessID from useraccess A, users B WHERE A.userName = B.userName AND B.userID = ?";
 $stmt = $pdo->prepare($sql);
 $stmt->execute([$userID]);
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
-$userAccessID = $row['userAccessID'];
+$accessLevel = $row['accessID'];
 
 $clubID = $_POST['viewClub'];
 $sql = "SELECT * FROM club WHERE clubID = ?";
@@ -34,88 +34,68 @@ while ($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {
     $photoURL = $row['url'];
 }
 ?>
-    <head>
-        <meta charset="UTF-8">
-        <title>Club Page</title>
-    </head>
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Club Page</title>
+</head><?php
 include("includes/header.php");
-echo"
-
+?>
 <body>
 <div class='ui container'>
-    <div class = 'ui grid'>
-        <div class = 'eight wide column'>
-            <header class = 'ui blue huge header'>$clubName Club Profile Page</header>
-        </div>
-
-        <div class = 'eight wide column'>
-        <form class = 'ui form' method = 'POST' action = 'editClubPage.php' onclick = '/semantic/src/inc/editClubPage.php'>
-            <button class = 'ui right floated button' onclick = '/semantic/src/inc/editClubPage.php' type = 'submit'>
-                <input type = 'hidden' name = 'editClub' value = $clubID>
-                <i class = 'ui settings icon'></i>
-        Edit Club
-        </button>
-            
-        </form>
-        </div>
-    </div>
     <div class='ui grid'>
-            <div class='four wide column'>
-                <div class='ui card'>
-                    <div class='image'>
-                        <img
-                            src='$photoURL'
-                    </div>
-                </div>
+        <div class='eight wide column'>
+            <header class='ui blue huge header'>
+                $clubName Club Profile Page
+            </header>
+        </div><?php
+        if($accessLevel == 1 || $accessLevel == 2){
+            echo "
+			                    <div class='eight wide column'>
+			            <form action='editClubPage.php' class='ui form' method='post' onclick='/semantic/src/inc/editClubPage.php'>
+			                <button class='ui right floated button' onclick='/semantic/src/inc/editClubPage.php' type='submit'><input name='editClub' type='hidden' value=\"$clubID\"> <i class='ui settings icon'></i> Edit Club</button>
+			            </form>
+			        </div>
+			            ";
+        }
+        ?>
+        <div class='four wide column'>
+            <div class='ui card'>
+                <div class='image'><img src='$photoURL'></div>
             </div>
-            <div class = 'twelve wide column'>
-            <div class='ui segment '>
-                <h5 class='ui top attached header '>
-                    Club Category:
-                </h5>
-                <div class='ui attached segment '>
+        </div>
+        <div class='twelve wide column'>
+            <div class='ui segment'>
+                <h5 class='ui top attached header'>Club Category:</h5>
+                <div class='ui attached segment'>
                     <p>$category</p>
                 </div>
-                <h5 class='ui attached header '>
-                    Club Description:
-                </h5>
-                <div class='ui attached segment '>
+                <h5 class='ui attached header'>Club Description:</h5>
+                <div class='ui attached segment'>
                     <p>$clubDesc</p>
                 </div>
-                <h5 class='ui attached header '>
-                    Contact Number:
-                </h5>
-                <div class='ui attached segment '>
+                <h5 class='ui attached header'>Contact Number:</h5>
+                <div class='ui attached segment'>
                     <p>$contactNum</p>
                 </div>
-                <h5 class='ui attached header '>
-                    Website URL:
-                </h5>
-                <div class='ui attached segment '>
+                <h5 class='ui attached header'>Website URL:</h5>
+                <div class='ui attached segment'>
                     <p>$websiteURL</p>
                 </div>
-                <h5 class='ui attached header '>
-                    Fee Required?
-                </h5>
-                <div class='ui attached segment '>
+                <h5 class='ui attached header'>Fee Required?</h5>
+                <div class='ui attached segment'>
                     <p>$feeRequired</p>
                 </div>
-                <h5 class='ui attached header '>
-                    Monthly Fee:
-                </h5>
-                <div class='ui attached segment '>
+                <h5 class='ui attached header'>Monthly Fee:</h5>
+                <div class='ui attached segment'>
                     <p>$feeCost</p>
                 </div>
             </div>
-            </div>
         </div>
-</div>
-</body>
-"
-?>
-
-<?php
+    </div>
+</div><?php
 include("includes/footer.php");
 ?>
+</body>
 </html>
