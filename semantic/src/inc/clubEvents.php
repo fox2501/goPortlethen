bbd1ab40455317719a3a4808776236e6ebf7cb75
Liam Stewart <liamstewart777@hotmail.co.uk>
2016-12-12 17:00:37 +0000

    create club Events

--- semantic/src/inc/clubEvents.php
+++ semantic/src/inc/clubEvents.php
@@ -84,11 +84,14 @@ $clubName = $row['clubName'];
                         <div class='seven wide column'>
                             <p id='mainText' style='text - align:justify'>$description<br></p>
                         </div>
+                        if($canAccess == 1){
                         <div class='two wide column'>
                             <form action='editClubEvent.php' class='ui form' method='post'>
                                 <button class='ui right floated mini button' onclick='/semantic/src/inc/editClubEvent.php' type='submit'><input name='editClub' type='hidden' value='$clubID'>Edit</button> 
                             </form>
                         </div>
+                        }
+                        
                     </div>
                 </div>
             </div>
