3b77a2738937338763dc2983888213a047bea75b
ross2106 <ross1994@hotmail.co.uk>
2016-12-12 10:44:00 +0000

    Home Page

--- semantic/Index.php
+++ semantic/Index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 //inserts header at top of page
 include("/src/inc/includes/header.php");
