3e70a641d98df4aab1b92168c7d51a31b7946b93
ross2106 <ross1994@hotmail.co.uk>
2016-12-12 13:35:41 +0000

    Club Pages

--- semantic/src/inc/createMap.php
+++ semantic/src/inc/createMap.php
@@ -11,7 +11,7 @@ $stmt -> execute([$userID]);
 $row = $stmt -> fetch(PDO::FETCH_ASSOC);
 $accessLevel = $row['accessID'];
 
-IF($accessLevel == 1 || $accessLevel == 3 || $accessLevel == 4) {
+if($accessLevel == 1 || $accessLevel == 3 || $accessLevel == 4) {
 
     include("includes/header.php");
     ?>
