78624dd380f5a94d5f0bddbf96ab02d5ec68bbff
ross2106 <ross1994@hotmail.co.uk>
2016-12-12 20:22:58 +0000

    Various changes

--- semantic/src/inc/healthForm.php
+++ semantic/src/inc/healthForm.php
@@ -6,116 +6,117 @@ include("includes/PDOConnect.php");
 if (isset($_SESSION['loggedIn'])) {
     $userID = $_SESSION['loggedIn'];
     $canAccess = '0';
-    $sql = "SELECT userName from users WHERE userID = ?";
+    $sql = "SELECT accessID from users U, useraccess UA WHERE U.userName = UA.userNamem AND userID = ?";
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$userID]);
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    $userName = $row["userName"];
+    $accessLevel = $row["accessID"];
 
-    $sql = "SELECT accessID from useraccess where userName = ? ";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute([$userName]);
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    $accessID = $row["accessID"];
-    if ($accessID == '1' || $accessID == '4') {
-        $canAccess = '1';
-    } else {
-        $canAccess = '0';
-    }
-    ?>
+    if ($accessLevel == 1 || $accessLevel == 4) {
 
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" type="text/css" href="semantic.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.js"></script>
-        <title>Semantic UI</title>
-    </head>
+        ?>
 
-    <!-- Nav bar -->
-    <? include("includes/header.php"); ?>
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <link rel="stylesheet" type="text/css" href="semantic.css">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.css">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.js"></script>
+            <title>Semantic UI</title>
+        </head>
 
-    <body>
-    <!-- Page header -->
-    <h1 align="center">Submit Health & Wellbeing Information</h1>
-    <div class="ui horizontal section divider">
-        Keeping Portlethen Healthy
-    </div>
+        <!-- Nav bar -->
+        <? include("includes/header.php"); ?>
 
-    <!-- Form -->
-    <div class="ui stackable container">
-        <form class="ui form" action="submitHealthForm.php" enctype="multipart/form-data" method="POST"
-              onsubmit="return validateForm()">
+        <body>
+        <!-- Page header -->
+        <h1 align="center">Submit Health & Wellbeing Information</h1>
+        <div class="ui horizontal section divider">
+            Keeping Portlethen Healthy
+        </div>
 
-            <div class="ui error message"></div>
+        <!-- Form -->
+        <div class="ui stackable container">
+            <form class="ui form" action="submitHealthForm.php" enctype="multipart/form-data" method="POST"
+                  onsubmit="return validateForm()">
 
-            <div class="required field">
-                <label>Title</label>
-                <div class="field">
-                    <input type="text" name="title" placeholder="Enter the title of your post">
+                <div class="ui error message"></div>
+
+                <div class="required field">
+                    <label>Title</label>
+                    <div class="field">
+                        <input type="text" name="title" placeholder="Enter the title of your post">
+                    </div>
                 </div>
-            </div>
 
 
-            <div class="required field">
-                <label>Content</label>
-                <textarea rows="8" type="text" name="mainText" placeholder="Enter the content of your post"></textarea>
-            </div>
-
-
-            <div class="required field">
-                <label>Main Image</label>
-                <div class="ui fluid action input">
-                    <input name="healthPhoto" size="35" type="file"/>
+                <div class="required field">
+                    <label>Content</label>
+                    <textarea rows="8" type="text" name="mainText"
+                              placeholder="Enter the content of your post"></textarea>
                 </div>
-            </div>
-            <button class="ui fluid large green submit button" id="createHealthContent" type="submit">Submit Content</button>
 
-            <script type="text/javascript">
-                ;
-                (function ($) {
-                    $('.ui.form').form({
-                        fields: {
-                            Title: {
-                                identifier: 'title',
-                                rules: [
-                                    {
-                                        type: 'empty',
-                                        prompt: 'Please enter a title'
-                                    }
-                                ]
-                            },
-                            mainText: {
-                                identifier: 'mainText',
-                                rules: [
-                                    {
-                                        type: 'empty',
-                                        prompt: 'Please enter text to populate the main text section'
-                                    }
-                                ]
-                            },
-                            photo: {
-                                identifier: 'healthPhoto',
-                                rules: [
-                                    {
-                                        type: 'empty',
-                                        prompt: 'Please choose a photo for the post'
-                                    }
-                                ]
+
+                <div class="required field">
+                    <label>Main Image</label>
+                    <div class="ui fluid action input">
+                        <input name="healthPhoto" size="35" type="file"/>
+                    </div>
+                </div>
+                <button class="ui fluid large green submit button" id="createHealthContent" type="submit">Submit
+                    Content
+                </button>
+
+                <script type="text/javascript">
+                    ;
+                    (function ($) {
+                        $('.ui.form').form({
+                            fields: {
+                                Title: {
+                                    identifier: 'title',
+                                    rules: [
+                                        {
+                                            type: 'empty',
+                                            prompt: 'Please enter a title'
+                                        }
+                                    ]
+                                },
+                                mainText: {
+                                    identifier: 'mainText',
+                                    rules: [
+                                        {
+                                            type: 'empty',
+                                            prompt: 'Please enter text to populate the main text section'
+                                        }
+                                    ]
+                                },
+                                photo: {
+                                    identifier: 'healthPhoto',
+                                    rules: [
+                                        {
+                                            type: 'empty',
+                                            prompt: 'Please choose a photo for the post'
+                                        }
+                                    ]
+                                }
                             }
-                        }
-                    })
-                })(jQuery);
-            </script>
+                        })
+                    })(jQuery);
+                </script>
 
-        </form>
-    </div>
-    </body>
-    <!-- Footerccc -->
-    <?php include("includes/footer.php"); ?>
+            </form>
+        </div>
+        </body>
+        <!-- Footerccc -->
+        <?php include("includes/footer.php"); ?>
 
-    </html>
-<?php }; ?>
+        </html>
+    <?php } else{
+        header("Location: /semantic/src/inc/health.php?restricted");
+    }
+} else{
+    header("Location: /semantic/src/inc/health.php?restricted");
+}
+; ?>
