c273f3291d6dfb747c5f5b76fb23fc478636d2be
ross2106 <ross1994@hotmail.co.uk>
2016-12-12 16:29:01 +0000

    Club Pages

--- semantic/src/inc/clubLandingPage.php
+++ semantic/src/inc/clubLandingPage.php
@@ -68,18 +68,18 @@ include("includes/header.php");
                 </div>
             </div>
 
-            <div class="four wide column">
-                <div class="ui fluid category search">
-                    <div class="ui right floated icon input">
-                        <form action="clubLandingPageSearched.php" method="post">
-                            <input class="prompt" name="search" placeholder="Search clubs..." type="text">
-                            <button type="submit">
-                                <i class="search icon"><?php header("Location: /semantic/src/inc/clubLandingPageSearched.php"); ?></i>
-                            </button>
-                        </form>
-                    </div>
-                </div>
-            </div>
+<!--            <div class="four wide column">-->
+<!--                <div class="ui fluid category search">-->
+<!--                    <div class="ui right floated icon input">-->
+<!--                        <form action="clubLandingPageSearched.php" method="post">-->
+<!--                            <input class="prompt" name="search" placeholder="Search clubs..." type="text">-->
+<!--                            <button type="submit">-->
+<!--                                <i class="search icon">--><?php //header("Location: /semantic/src/inc/clubLandingPageSearched.php"); ?><!--</i>-->
+<!--                            </button>-->
+<!--                        </form>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
 
         </div>
     </div>
