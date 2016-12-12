60b9dca29fea040e7373ce894ac6d30bca9e6450
ross2106 <ross1994@hotmail.co.uk>
2016-12-12 16:15:24 +0000

    Club Pages

--- semantic/src/inc/clubLandingPage.php
+++ semantic/src/inc/clubLandingPage.php
@@ -1,27 +1,27 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 //session begins
 session_start();
 //connects to database server
-include("includes/PDOConnect.php");
 include("includes/dbconnect.php");
 $url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 //access levels
+if (isset($_SESSION['loggedIn'])) {
     $userID = $_SESSION['loggedIn'];
     $canAccess = '0';
-    $sql = "SELECT accessID from users U, useraccess UA WHERE U.userName = UA.userName AND userID = ?";
-    $stmt = $pdo -> prepare($sql);
-    $stmt -> execute([$userID]);
-    $row = $stmt -> fetch(PDO::FETCH_ASSOC);
-    $userName = $row['userName'];
-    $accessLevel = $row['accessID'];
-    if ($accessLevel == 1 || $accessLevel == 2) {
-        $canAccess = 1;
+    $sql = "SELECT userName from users WHERE userID = '$userID'";
+    $result = mysqli_query($db, $sql);
+    $row = mysqli_fetch_assoc($result);
+    $userName = $row["userName"];
+    $sql = "SELECT accessID from useraccess where userName = '$userName'";
+    $result = mysqli_query($db, $sql);
+    $row = mysqli_fetch_assoc($result);
+    $accessID = $row["accessID"];
+    if ($accessID == '1' || $accessID == '2') {
+        $canAccess = '1';
     } else {
-        $canAccess = 0;
+        $canAccess = '0';
     }
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -74,18 +74,18 @@ include("includes/header.php");
                 </div>
             </div>
 
-<!--            <div class="four wide column">-->
-<!--                <div class="ui fluid category search">-->
-<!--                    <div class="ui right floated icon input">-->
-<!--                        <form action="clubLandingPageSearched.php" method="post">-->
-<!--                            <input class="prompt" name="search" placeholder="Search clubs..." type="text">-->
-<!--                            <button type="submit">-->
-<!--                                <i class="search icon">--><?php //header("Location: /semantic/src/inc/clubLandingPageSearched.php"); ?><!--</i>-->
-<!--                            </button>-->
-<!--                        </form>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </div>-->
+            <div class="four wide column">
+                <div class="ui fluid category search">
+                    <div class="ui right floated icon input">
+                        <form action="clubLandingPageSearched.php" method="post">
+                            <input class="prompt" name="search" placeholder="Search clubs..." type="text">
+                            <button type="submit">
+                                <i class="search icon"><?php header("Location: /semantic/src/inc/clubLandingPageSearched.php"); ?></i>
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
 
         </div>
     </div>
@@ -95,9 +95,9 @@ include("includes/header.php");
 <div class="ui stackable container">
     <div class="ui stackable grid">
         <div class="ui hidden divider"></div><?php
-        $sql = "SELECT A.clubName, A.clubDescription, A.clubID, B.url FROM club A, photos B WHERE A.clubID = B.clubID";
-        $result = mysqli_query($db, $sql);
-        while ($row = mysqli_fetch_assoc($result)) {
+        $sql_query = "SELECT A.clubName, A.clubDescription, A.clubID, B.url FROM club A, photos B WHERE A.clubID = B.clubID";
+        $result = $db->query($sql_query);
+        while ($row = $result->fetch_array()) {
             $title = $row['clubName'];
             $mainText = $row['clubDescription'];
             $clubID = $row['clubID'];
