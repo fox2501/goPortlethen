0732713ecbf0cc65c7ea0f11d4b139108e76cf51
ross2106 <ross1994@hotmail.co.uk>
2016-12-12 10:33:27 +0000

    Home Page

--- semantic/Index.php
+++ semantic/Index.php
@@ -4,6 +4,20 @@ session_start();
 include("/src/inc/includes/header.php");
 $url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 
+$sql = "SELECT * FROM homeContent";
+$stmt = $pdo -> prepare($sql);
+$stmt -> execute();
+$row = $stmt -> fetch(PDO::FETCH_ASSOC);
+$contentID = $row['contentID'];
+$title = $row['title'];
+$caption = $row['caption'];
+
+$sql = "SELECT * FROM photos WHERE homeContentID = ?";
+$stmt = $pdo -> prepare($sql);
+$stmt -> execute([$contentID]);
+$row = $stmt -> fetch(PDO::FETCH_ASSOC);
+$photoID = $row['photoID'];
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -35,20 +49,19 @@ if(strpos($url, 'accountDeleted') !== false){
     <div class="ui stackable grid">
         <div class="sixteen wide column">
             <form action='editHomePage.php' class='ui form' method='post' onclick='/semantic/src/inc/editHomePage.php'>
-                <button class='ui right floated button' onclick='/semantic/src/inc/editHomePage.php' type='submit'><input name='editHome' type='hidden'> <i class='ui settings icon'></i> Edit Home</button>
+                <button class='ui right floated button' onclick='/semantic/src/inc/editHomePage.php' type='submit'><input name='editHome' type='hidden' value = '<?php echo $contentID ?>'> <i class='ui settings icon'></i> Edit Home</button>
             </form>
         </div>
         <div class="sixteen wide column">
             <div class="ui huge blue centered header">
-                Welcome to goPortlethen.org.uk
+                <?php echo $title ?>
             </div>
         </div>
         <div class="sixteen wide column">
-            <div class="column"><img class="ui medium centered circular image" src="https://scontent.flhr4-1.fna.fbcdn.net/v/t1.0-9/13434842_1608517786105160_4523080997776743356_n.jpg?oh=4981b2761c2ef40c4989fc4b74bd440a&oe=58C6B38A"></div>
+            <div class="column"><img class="ui medium centered circular image" src="<?php echo $photoID ?>"></div>
         </div>
         <div class="sixteen wide column">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eros est, feugiat sed ex ac, ultrices pretium elit. Nunc vel varius lectus. Nullam placerat erat ipsum, non consectetur nisi aliquam id. Aliquam in libero leo. Nam et neque mi. Sed vulputate blandit nisl quis feugiat. Morbi tempus commodo dui, et sagittis turpis porttitor vitae. Mauris pharetra id arcu ac efficitur. Mauris id mauris sit amet erat faucibus luctus. Morbi auctor dictum fringilla. Ut ullamcorper lorem nisi, scelerisque consequat neque volutpat vel. Duis eget ante nec felis varius interdum. Duis viverra orci nibh, et tincidunt mauris rhoncus et. Aenean elementum faucibus gravida. Aenean sed consectetur lacus. Nam imperdiet ultricies rhoncus. Ut sed feugiat lacus. Duis ac nulla ac turpis facilisis sollicitudin ut sit amet odio. Etiam vulputate ipsum nec pretium interdum. Nam vitae eros aliquet, aliquam nisl in, mollis nisl. Ut tristique, justo ut hendrerit varius, urna ex luctus erat, eget varius nulla tellus a orci. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed blandit nulla sit amet odio elementum efficitur.</p>
-        </div>
+            <p><?php echo $caption ?></p>
         <div class="ui header">
             Need help? Click <a href="/semantic/src/inc/help.php">here</a> for FAQs!
         </div>
---
+++ semantic/src/inc/editHome.php
@@ -0,0 +1,108 @@
+<?php
+session_start();
+//connects to database server
+include("includes/PDOConnect.php");
+if (isset($_SESSION['loggedIn'])) {
+$userID = $_SESSION['loggedIn'];
+$sql = "SELECT A.accessID from useraccess A, users B WHERE A.userName = B.userName AND B.userID = ?";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([$userID]);
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+$accessLevel = $row['accessID'];
+
+if ($accessLevel == 1) {
+$sql = "SELECT * from homeContent WHERE $contentID = ?";
+$stmt = $pdo -> prepare($sql);
+$stmt -> execute([$contentID]);
+while ($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {
+    $title = $row['title'];
+    $mainText = $row['caption'];
+}
+?>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="semantic.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.js"></script>
+</head>
+<!-- Nav bar -->
+<? include("includes/header.php"); ?>
+<body>
+<!--main body-->
+<!-- Form -->
+
+
+<div class="ui container">
+    <h1 align="center">
+        Edit Health & Wellbeing Content
+    </h1>
+    <div class="ui horizontal section divider">
+        <p>Keeping Portlethen Healthy</p>
+    </div>
+
+    <form method ="post" action ="deleteHealth.php">
+        <button class="ui red right floated button" type ="submit" name = "healthID" value = "<?php echo $healthContentID; ?>">Delete Health Content</button>
+    </form>
+
+    <br>
+
+    <form action="submitEditHealth.php" class="ui form" enctype="multipart/form-data" method="post">
+        <div class="field">
+            <label>Edit Title</label> <input name="editTitle" type="text" value="<?php echo $title; ?>">
+        </div>
+        <div class="field">
+            <label>Edit Main Text</label>
+            <textarea name="editMainText" rows="8"><?php echo $mainText; ?></textarea>
+        </div>
+        <div class="field">
+            <input name="healthContentToEdit" type="hidden" value="<?php echo $healthContentID ?>">
+        </div>
+        <div class="field">
+            <label>Choose a new image for the content</label>
+            <div class="ui fluid action input">
+                <input name="healthPhoto" size="35" type="file">
+            </div>
+        </div><button class="ui fluid large green submit button" type="submit">Submit</button>
+
+        <script type="text/javascript">
+            ;
+            (function ($) {
+                $('.ui.form').form({
+                    fields: {
+                        Title: {
+                            identifier: 'title',
+                            rules: [
+                                {
+                                    type: 'empty',
+                                    prompt: 'Please enter a title'
+                                }
+                            ]
+                        },
+                        mainText: {
+                            identifier: 'mainText',
+                            rules: [
+                                {
+                                    type: 'empty',
+                                    prompt: 'Please enter text to populate the main text section'
+                                }
+                            ]
+                        }
+                    }
+                })
+            })(jQuery);
+        </script>
+    </form>
+
+</div>
+
+
+<!-- Footer -->
+<?php
+include("includes/footer.php");
+} else {
+    echo "You do not have permission.";
+}
+};
+?>
+</body>
