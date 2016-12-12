b03c20b42eac0de9e4fd22cc6dbc22f9015539ea
ross2106 <ross1994@hotmail.co.uk>
2016-12-12 15:31:48 +0000

    Club Pages

--- semantic/src/inc/submitHelp.php
+++ semantic/src/inc/submitHelp.php
@@ -18,7 +18,7 @@ $stmt->execute([1]);
 $emailString;
 while($row = $stmt -> fetch(PDO::FETCH_ASSOC)){
 
-    echo $row;
+    echo $row['emailAddress'];
 //$emailString = $emailString.$row['emailAddress'].";";
 
 }
