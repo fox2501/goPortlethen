c90d768325b417afa8c22ce64580d8f7a59f125f
ross2106 <ross1994@hotmail.co.uk>
2016-12-12 10:45:12 +0000

    Home Page

--- semantic/Index.php
+++ semantic/Index.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 session_start();
 //inserts header at top of page
 include("/src/inc/includes/header.php");
-include("includes/PDOConnect");
+include("/src/inc/includes/PDOConnect");
 $url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 
 $sql = "SELECT * FROM homecontent";
