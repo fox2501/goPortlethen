6bcfbf093fba8e0c11dc32d54c116b630d73449d
ross2106 <ross1994@hotmail.co.uk>
2016-12-12 11:58:28 +0000

    Home Page

--- semantic/src/inc/clubPage.php
+++ semantic/src/inc/clubPage.php
@@ -68,20 +68,20 @@ if (isset($_SESSION['loggedIn'])) {
 <body>
 <div class='ui stackable container'>
     <div class='ui stackable grid'>
-        <div class='eight wide column'>
-            <header class='ui blue huge header'>
-                <?php echo $clubName ?> Club Profile Page
-            </header>
-        </div>
         <?php
         if ($canAccess == 1){
             echo"
-	<div class='eight wide column'>
+	<div class='sixteen wide column'>
 		<form action='editClubPage.php' class='ui form' method='post'>
 			<button class='ui right floated button' type='submit'><input name='editClub' type='hidden' value=\"$clubID\"> <i class='ui settings icon'></i> Edit Club</button>
 		</form>
 	</div>";
-                }?>
+        }?>
+        <div class='sixteen wide column'>
+            <header class='ui blue huge header'>
+                <?php echo $clubName ?> Club Profile Page
+            </header>
+        </div>
     <div class='ui stackable grid'>
         <div class='four wide column'>
             <div class='ui card'>
