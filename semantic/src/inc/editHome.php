2ffc8f5f4e4f39a4853fa342523532c652ea82e0
ross2106 <ross1994@hotmail.co.uk>
2016-12-12 10:54:18 +0000

    Home Page

--- semantic/src/inc/editHome.php
+++ semantic/src/inc/editHome.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 session_start();
 //connects to database server
 include("includes/PDOConnect.php");
@@ -21,28 +24,27 @@ while ($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {
     $caption = $row['caption'];
 }
 ?>
+<!DOCTYPE html>
+<html>
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="semantic.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.js"></script>
-</head>
-<!-- Nav bar -->
+    <link href="semantic.css" rel="stylesheet" type="text/css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.js">
+    </script>
+    <title></title>
+</head><!-- Nav bar -->
 <? include("includes/header.php"); ?>
 <body>
 <!--main body-->
 <!-- Form -->
-
-
 <div class="ui container">
-    <h1 align="center">
-        Edit Home Page Content
-    </h1>
+    <h1 align="center">Edit Home Page Content</h1>
     <div class="ui horizontal section divider">
         <p>Give users a good first impression!</p>
     </div>
-
     <form action="submitEditHome.php" class="ui form" enctype="multipart/form-data" method="post">
         <div class="field">
             <label>Edit Title</label> <input name="editTitle" type="text" value="<?php echo $title; ?>">
@@ -60,7 +62,6 @@ while ($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {
                 <input name="healthPhoto" size="35" type="file">
             </div>
         </div><button class="ui fluid large green submit button" type="submit">Submit</button>
-
         <script type="text/javascript">
             ;
             (function ($) {
@@ -89,11 +90,7 @@ while ($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {
             })(jQuery);
         </script>
     </form>
-
-</div>
-
-
-<!-- Footer -->
+</div><!-- Footer -->
 <?php
 include("includes/footer.php");
 } else {
@@ -102,3 +99,4 @@ include("includes/footer.php");
 };
 ?>
 </body>
+</html>
