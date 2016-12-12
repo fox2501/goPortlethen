600d88ae01c76154eead14cdc1094ad93c1be183
ross2106 <ross1994@hotmail.co.uk>
2016-12-12 20:14:43 +0000

    Various changes

--- semantic/src/inc/editClubPage.php
+++ semantic/src/inc/editClubPage.php
@@ -12,12 +12,14 @@ $stmt->execute([$userID]);
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
 $accessID = $row['accessID'];
 
+if($accessID == 1 || $accessID == 2){
+
 $clubID = $_POST['editClub'];
 $sql = "SELECT userID from club where clubID = ?";
 $stmt = $pdo -> prepare($sql);
 $stmt->execute([$clubID]);
 while ($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {
-if ($row['userID'] == $userID || $accessID == '1') {
+if ($row['userID'] == $userID || $accessID == 1) {
 $clubID = $_POST['editClub'];
 $sql = "SELECT * FROM club WHERE clubID = ?";
 $stmt = $pdo -> prepare($sql);
@@ -48,150 +50,157 @@ while ($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {
 <head>
     <meta charset="UTF-8">
     <title>Edit Club</title>
-</head>
-<?php
+</head><?php
 include("includes/header.php");
 echo "
 <body>
 <div class='ui stackable container'>
-		<div class='ui stackable grid'>
-			<div class='eight wide column'>
-				<header class='ui blue huge header'>
-					Edit club: $clubName
-				</header>
-			</div>
-			<div class='eight wide column'>
-				<form action='deleteClub.php' method='post'>
-					<button class='ui red right floated button' name='clubID' type='submit' value='$clubID'>Delete Club</button>
-				</form>
-			</div>
-			<div class='four wide column'>
-				<div class='ui card'>
-					<div class='image'><img src='$photoURL'></div>
-				</div>
-			</div>
-			<div class='twelve wide column'>
-				<form action='submitEditClub.php' class='ui form' enctype=\"\&quot;multipart/form-data\&quot;\" method='post'>
-					<div class='field'>
-						<label>Edit Club Name</label> <input name='editClubName' type='text' value='$clubName'>
-					</div>
-					<div class='field'>
-						<label>Edit Club Description</label> 
-						<textarea name='editDescription' rows=\"\&quot;\&quot;8\&quot;\&quot;\">$clubDesc</textarea>
-					</div>
-					<div class='field'>
-						<label>Edit Contact Number</label> <input name='editNumber' type='number' value='$contactNum'>
-					</div>
-					<div class='field'>
-						<label>Edit Website</label> <input name='editURL' type='text' value='$websiteURL'>
-					</div>
-					<div class='field'>
-						<label>Fee Required?</label>
-						<div class='ui selection dropdown' id='dropdown'>
-							<input name='editFeeRequired' type='hidden'> <i class='ui dropdown icon'></i>
-							<div class='default text'>
-								$feeRequired
-							</div>
-							<div class='menu'>
-								<div class='ui simple dropdown item'>
-									Yes
-								</div>
-								<div class='ui simple downdown item'>
-									No
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class='field'>
-						<label>Edit Monthly Cost</label> <input name='editCost' type='text' value='$feeCost'>
-					</div>
-					<div class='field'>
-						<input name='clubToEdit' type='hidden' value='$clubID'>
-					</div>
-					<div class='field'>
-						<label>Please upload your clubs profile picture:</label>
-						<div class='ui fluid action input'>
-							<input name='img' size='35' type='file'>
-						</div>
-					</div><button class='ui fluid large green submit button' type='submit'>Submit</button> 
-					<script type='text/javascript'>
-					               
-					               (function ($) {
-					                   $('.ui.form').form({
-					                       fields: {
-					                           clubName: {
-					                               identifier: 'clubName',
-					                               rules: [
-					                                   {
-					                                       type: 'empty',
-					                                       prompt: 'Please enter your clubs name'
-					                                   }
-					                               ]
-					                           },
-					                           clubEmail: {
-					                               identifier: 'email',
-					                               rules: [
-					                                   {
-					                                       type: 'email',
-					                                       prompt: 'Please enter a valid email address'
-					                                   }
-					                               ]
-					                           },
-					                           clubCategory: {
-					                               identifier: 'clubCategory',
-					                               rules: [
-					                                   {
-					                                       type: 'empty',
-					                                       prompt: 'Please select a club category'
-					                                   }
-					                               ]
-					                           },
-					                           clubDescription: {
-					                               identifier: 'clubDescription',
-					                               rules: [
-					                                   {
-					                                       type: 'empty',
-					                                       prompt: 'Please select a club category'
-					                                   }
-					                               ]
-					                           },
-					                           number: {
-					                               identifier: 'phoneNumber',
-					                               rules: [
-					                                   {
-					                                       type: 'number',
-					                                       prompt: 'Please enter a valid phone number'
-					                                   }
-					                               ]
-					                           },
-					                           url: {
-					                               identifier: 'websiteURL',
-					                               optional   : true,
-					                               rules: [
-					                                   {
-					                                       type: 'url',
-					                                       prompt: 'Please enter a url'
-					                                   }
-					                               ]
-					                           }
-					                       }
-					                   })
-					               })(jQuery);
-					</script>
-				</form>
-			</div>
-		</div>
-	</div>
+        <div class='ui stackable grid'>
+            <div class='eight wide column'>
+                <header class='ui blue huge header'>
+                    Edit club: $clubName
+                </header>
+            </div>
+            <div class='eight wide column'>
+                <form action='deleteClub.php' method='post'>
+                    <button class='ui red right floated button' name='clubID' type='submit' value='$clubID'>Delete Club</button>
+                </form>
+            </div>
+            <div class='four wide column'>
+                <div class='ui card'>
+                    <div class='image'><img src='$photoURL'></div>
+                </div>
+            </div>
+            <div class='twelve wide column'>
+                <form action='submitEditClub.php' class='ui form' enctype=\"\&quot;multipart/form-data\&quot;\" method='post'>
+                    <div class='field'>
+                        <label>Edit Club Name</label> <input name='editClubName' type='text' value='$clubName'>
+                    </div>
+                    <div class='field'>
+                        <label>Edit Club Description</label> 
+                        <textarea name='editDescription' rows=\"\&quot;\&quot;8\&quot;\&quot;\">$clubDesc</textarea>
+                    </div>
+                    <div class='field'>
+                        <label>Edit Contact Number</label> <input name='editNumber' type='number' value='$contactNum'>
+                    </div>
+                    <div class='field'>
+                        <label>Edit Website</label> <input name='editURL' type='text' value='$websiteURL'>
+                    </div>
+                    <div class='field'>
+                        <label>Fee Required?</label>
+                        <div class='ui selection dropdown' id='dropdown'>
+                            <input name='editFeeRequired' type='hidden'> <i class='ui dropdown icon'></i>
+                            <div class='default text'>
+                                $feeRequired
+                            </div>
+                            <div class='menu'>
+                                <div class='ui simple dropdown item'>
+                                    Yes
+                                </div>
+                                <div class='ui simple downdown item'>
+                                    No
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class='field'>
+                        <label>Edit Monthly Cost</label> <input name='editCost' type='text' value='$feeCost'>
+                    </div>
+                    <div class='field'>
+                        <input name='clubToEdit' type='hidden' value='$clubID'>
+                    </div>
+                    <div class='field'>
+                        <label>Please upload your clubs profile picture:</label>
+                        <div class='ui fluid action input'>
+                            <input name='img' size='35' type='file'>
+                        </div>
+                    </div><button class='ui fluid large green submit button' type='submit'>Submit</button> 
+                    <script type='text/javascript'>
+                                   
+                                   (function ($) {
+                                       $('.ui.form').form({
+                                           fields: {
+                                               clubName: {
+                                                   identifier: 'clubName',
+                                                   rules: [
+                                                       {
+                                                           type: 'empty',
+                                                           prompt: 'Please enter your clubs name'
+                                                       }
+                                                   ]
+                                               },
+                                               clubEmail: {
+                                                   identifier: 'email',
+                                                   rules: [
+                                                       {
+                                                           type: 'email',
+                                                           prompt: 'Please enter a valid email address'
+                                                       }
+                                                   ]
+                                               },
+                                               clubCategory: {
+                                                   identifier: 'clubCategory',
+                                                   rules: [
+                                                       {
+                                                           type: 'empty',
+                                                           prompt: 'Please select a club category'
+                                                       }
+                                                   ]
+                                               },
+                                               clubDescription: {
+                                                   identifier: 'clubDescription',
+                                                   rules: [
+                                                       {
+                                                           type: 'empty',
+                                                           prompt: 'Please select a club category'
+                                                       }
+                                                   ]
+                                               },
+                                               number: {
+                                                   identifier: 'phoneNumber',
+                                                   rules: [
+                                                       {
+                                                           type: 'number',
+                                                           prompt: 'Please enter a valid phone number'
+                                                       }
+                                                   ]
+                                               },
+                                               url: {
+                                                   identifier: 'websiteURL',
+                                                   optional   : true,
+                                                   rules: [
+                                                       {
+                                                           type: 'url',
+                                                           prompt: 'Please enter a url'
+                                                       }
+                                                   ]
+                                               }
+                                           }
+                                       })
+                                   })(jQuery);
+                    </script>
+                </form>
+            </div>
+        </div>
+    </div>
     ";
-    include('includes/footer.php');
+include('includes/footer.php');
 echo "
 </body>
 ";
 
 } else {
-    echo "You do not have permission";
+    header("Location: /semantic/src/inc/clubLandingPage.php?restricted");
 }
 }
-};
+
+}else{
+    header("Location: /semantic/src/inc/logIn.php?restricted");
+}
+}else{
+    header("Location: /semantic/src/inc/logIn.php?restricted");
+}
 ?>
+<body>
+</body>
 </html>
